60e865450992eb04cd77b0eff220c87c922c773c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 15:15:47 +0330

    added clubadmin entity.

---
+++ src/Zazz.Core/Models/Data/ClubAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class ClubAdmin
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("ClubId")]
+        public Club Club { get; set; }
+
+        public int ClubId { get; set; }
+
+    }
+}
