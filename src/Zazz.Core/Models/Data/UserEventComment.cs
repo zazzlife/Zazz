d3930e37577a5de1f5c8479fa506999051ca90f8
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 16:49:06 +0330

    added user event comment entity.

---
+++ src/Zazz.Core/Models/Data/UserEventComment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class UserEventComment : BaseEntity
+    {
+        [ForeignKey("FromId")]
+        public User From { get; set; }
+
+        public int FromId { get; set; }
+
+        [ForeignKey("UserEventId")]
+        public UserEvent UserEvent { get; set; }
+
+        public int UserEventId { get; set; }
+
+        [MaxLength(1000)]
+        public string Message { get; set; } 
+    }
+}
