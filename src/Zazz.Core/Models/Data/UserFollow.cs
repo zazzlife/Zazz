daad3a2f39c91823d65b74e541e96a669de69cbf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 16:25:27 +0330

    added user follow entity.

---
+++ src/Zazz.Core/Models/Data/UserFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class UserFollow : BaseEntity
+    {
+        /// <summary>
+        /// This is the user that follows another user
+        /// </summary>
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        /// <summary>
+        /// This is the user that follows another user
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// This is the user that is being followed
+        /// </summary>
+        [ForeignKey("FollowId")]
+        public User Follow { get; set; }
+
+        /// <summary>
+        /// This is the user that is being followed
+        /// </summary>
+        public int FollowId { get; set; }
+    }
+}
