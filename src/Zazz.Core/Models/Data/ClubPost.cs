2258eb7f8d1ff1e108e5c6c61b6d8abe975b54a7
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 16:42:22 +0330

    removed club event, using a post as both post and event.

--- src/Zazz.Core/Models/Data/ClubPost.cs
+++ src/Zazz.Core/Models/Data/ClubPost.cs
@@ -1,7 +1,38 @@
-﻿namespace Zazz.Core.Models.Data
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
 {
-    public class ClubPost : BaseClubPost
+    public class ClubPost : BaseEntity
     {
-         
+        [ForeignKey("ClubId")]
+        public Club Club { get; set; }
+
+        public int ClubId { get; set; }
+
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [MaxLength(255)]
+        public string PictureLink { get; set; }
+
+        [MaxLength(255)]
+        public string VideoLink { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        [MaxLength(50)]
+        public string FacebookItemId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        [MaxLength(100)]
+        public string Location { get; set; }
     }
 }
