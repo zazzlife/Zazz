8d0820598e0040acd6e01d6682860b123a026313
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 16:47:43 +0330

    added club post comment entity.

---
+++ src/Zazz.Core/Models/Data/ClubPostComment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class ClubPostComment : BaseEntity
+    {
+        [ForeignKey("FromId")]
+        public User From { get; set; }
+
+        public int FromId { get; set; }
+
+        [ForeignKey("ClubId")]
+        public Club Club { get; set; }
+
+        public int ClubId { get; set; }
+
+        [MaxLength(1000)]
+        public string Message { get; set; }
+    }
+}
