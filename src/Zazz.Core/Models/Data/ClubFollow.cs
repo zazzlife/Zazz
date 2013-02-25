4e4d82f7e4d198a894f806d7762805f40ad158e3
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 16:16:58 +0330

    added club follow entity.

---
+++ src/Zazz.Core/Models/Data/ClubFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class ClubFollow : BaseEntity
+    {
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("ClubId")]
+        public Club Club { get; set; }
+
+        public int ClubId { get; set; }
+    }
+}
