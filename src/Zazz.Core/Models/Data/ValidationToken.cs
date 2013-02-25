54d4146ed25445480afe506c7109b08c65cb951c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 20:08:17 +0330

    fixed validation token one to one problem.

--- src/Zazz.Core/Models/Data/ValidationToken.cs
+++ src/Zazz.Core/Models/Data/ValidationToken.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Zazz.Core.Models.Data
 {
     public class ValidationToken : BaseEntity
     {
+        [ForeignKey("Id")]
+        public User User { get; set; }
+
         public int Token { get; set; }
 
         public DateTime ExpirationDate { get; set; }
