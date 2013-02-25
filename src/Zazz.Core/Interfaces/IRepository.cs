d592a80463b3da0270c469a1c4c8b59487d40422
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-25 18:03:49 +0330

    added repository interface

---
+++ src/Zazz.Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void InsertGraph(T item);
+
+        void InsertOrUpdate(T item);
+
+        Task<T> GetById(int id);
+
+        void Delete(int id);
+
+        void Delete(T item);
+    }
+}
