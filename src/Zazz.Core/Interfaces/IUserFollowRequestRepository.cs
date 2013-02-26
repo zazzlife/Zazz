9e57b0e930ce31b6cfddd5a32a66db7ac2042c93
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 15:11:16 +0330

    added user follow request repo.

---
+++ src/Zazz.Core/Interfaces/IUserFollowRequestRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IUserFollowRequestRepository : IRepository<UserFollowRequest>
+    {
+        Task<IList<UserFollowRequest>> GetReceivedRequests(int userId);
+
+        Task<IList<UserFollowRequest>> GetSentRequests(int userId);
+
+        Task Remove(int fromUserId, int toUserId);
+    }
+}
---
+++ src/Zazz.Data/Repositories/UserFollowRequestRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class UserFollowRequestRepository : BaseRepository<UserFollowRequest>, IUserFollowRequestRepository
+    {
+        public UserFollowRequestRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override int GetItemId(UserFollowRequest item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<UserFollowRequest>> GetReceivedRequests(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<UserFollowRequest>> GetSentRequests(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Remove(int fromUserId, int toUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
