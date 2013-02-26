f75738001dd42442067c5632dcc7bb72094ba3b9
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 16:48:33 +0330

    implemented user follow repo

---
+++ src/Zazz.Core/Interfaces/IUserFollowRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IUserFollowRepository : IRepository<UserFollow>
+    {
+        /// <summary>
+        /// Returns a list of users that follow the user
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<UserFollow>> GetUserFollowersAsync(int toUserId);
+
+        /// <summary>
+        /// Returns a list of users that the user follows
+        /// </summary>
+        /// <param name="fromUserId"></param>
+        /// <returns></returns>
+        Task<IEnumerable<UserFollow>> GetUserFollowsAsync(int fromUserId);
+
+        Task<bool> ExistsAsync(int fromUserId, int toUserId);
+
+        Task DeleteAsync(int fromUserId, int toUserId);
+    }
+}
---
+++ src/Zazz.Data/Repositories/UserFollowRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class UserFollowRepository : BaseRepository<UserFollow>, IUserFollowRepository
+    {
+        public UserFollowRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override int GetItemId(UserFollow item)
+        {
+            if (item.FromUserId == default (int) || item.ToUserId == default (int))
+                throw new ArgumentException("FromUserId or ToUserId cannot be 0");
+
+            return DbSet.Where(f => f.FromUserId == item.FromUserId)
+                        .Where(f => f.ToUserId == item.ToUserId)
+                        .Select(f => f.Id)
+                        .SingleOrDefault();
+        }
+
+        public Task<IEnumerable<UserFollow>> GetUserFollowersAsync(int toUserId)
+        {
+            return Task.Run(() => DbSet.Where(f => f.ToUserId == toUserId).AsEnumerable());
+        }
+
+        public Task<IEnumerable<UserFollow>> GetUserFollowsAsync(int fromUserId)
+        {
+            return Task.Run(() => DbSet.Where(f => f.FromUserId == fromUserId).AsEnumerable());
+        }
+
+        public Task<bool> ExistsAsync(int fromUserId, int toUserId)
+        {
+            return Task.Run(() => DbSet.Where(f => f.FromUserId == fromUserId)
+                                       .Where(f => f.ToUserId == toUserId)
+                                       .Any());
+        }
+
+        public async Task DeleteAsync(int fromUserId, int toUserId)
+        {
+            var item = await Task.Run(() => DbSet.Where(f => f.FromUserId == fromUserId)
+                                                 .Where(f => f.ToUserId == toUserId)
+                                                 .SingleOrDefault());
+
+            if (item == null)
+                return;
+
+            DbContext.Entry(item).State = EntityState.Deleted;
+        }
+    }
+}
