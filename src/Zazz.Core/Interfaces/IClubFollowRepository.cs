761aa6bced6a070c744c023885c6592f9780e6e5
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 21:01:51 +0330

    implemented club follow repository.

---
+++ src/Zazz.Core/Interfaces/IClubFollowRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IClubFollowRepository : IRepository<ClubFollow>
+    {
+        Task<bool> ExistsAsync(int userId, int clubId);
+
+        Task RemoveAsync(int userId, int clubId);
+    }
+}
