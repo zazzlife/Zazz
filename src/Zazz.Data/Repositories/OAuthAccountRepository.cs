69d56f110d32c1af9df05656bc63a7211afd5e9c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 13:25:08 +0330

    added oauth account repo.

---
+++ src/Zazz.Data/Repositories/OAuthAccountRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class OAuthAccountRepository : BaseRepository<OAuthAccount>, IOAuthAccountRepository
+    {
+        public OAuthAccountRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override int GetItemId(OAuthAccount item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<OAuthAccount> GetUserAccount(int userId, OAuthProvider provider)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<OAuthAccount>> GetUserAccounts(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int userId, OAuthProvider provider)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
