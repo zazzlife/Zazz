765fa42564eaa9f2e5d41b1f8032b6b9eb5af045
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 17:32:18 +0330

    added user event repo interface.

---
+++ src/Zazz.Core/Interfaces/IUserEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IUserEventRepository : IRepository<UserEvent>
+    {
+        Task<bool> IsAuthorizedToUpdate(int eventId, int userId);
+    }
+}
