71131c13f739923e9c770a2833cd581c5b9e0b91
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 22:03:55 +0330

    added club post comment repo.

---
+++ src/Zazz.Core/Interfaces/IClubPostCommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IClubPostCommentRepository : IRepository<ClubPostComment>
+    {
+        Task<IQueryable<ClubPostComment>> GetPostCommentsAsync(int postId);
+    }
+}
