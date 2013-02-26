77bd1ad1ebbff97e5b745db85a540a52ce5177fa
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 16:07:21 +0330

    user fowllow requests repo - testing 2

--- tests/Zazz.IntegrationTests/Repositories/UserFollowRequestRepositoryShould.cs
+++ tests/Zazz.IntegrationTests/Repositories/UserFollowRequestRepositoryShould.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using NUnit.Framework;
 using Zazz.Core.Models.Data;
 using Zazz.Data;
@@ -20,17 +21,17 @@ namespace Zazz.IntegrationTests.Repositories
         }
 
         [Test]
-        public void ShouldThrowException_OnInsertOrUpdate_WhenFromUserIdIs0()
+        public void ThrowException_OnInsertOrUpdate_WhenFromUserIdIs0()
         {
             //Arrange
-            var followRequest = new UserFollowRequest {ToUserId = 21, RequestDate = DateTime.Now};
+            var followRequest = new UserFollowRequest { ToUserId = 21, RequestDate = DateTime.Now };
 
             //Act & Assert
             Assert.Throws<ArgumentException>(() => _repo.InsertOrUpdate(followRequest));
         }
 
         [Test]
-        public void ShouldThrowException_OnInsertOrUpdate_WhenToUserIdIs0()
+        public void ThrowException_OnInsertOrUpdate_WhenToUserIdIs0()
         {
             //Arrange
             var followRequest = new UserFollowRequest { FromUserId = 21, RequestDate = DateTime.Now };
@@ -38,5 +39,93 @@ namespace Zazz.IntegrationTests.Repositories
             //Act & Assert
             Assert.Throws<ArgumentException>(() => _repo.InsertOrUpdate(followRequest));
         }
+
+        [Test]
+        public void GetUserReceivedFollowRequests_OnGetReceivedRequestsAsync()
+        {
+            //Arrange (user B and C will send a request to user A)
+            var userA = Mother.GetUser();
+            var userB = Mother.GetUser();
+            var userC = Mother.GetUser();
+
+            using (var ctx = new ZazzDbContext())
+            {
+                ctx.Users.Add(userA);
+                ctx.Users.Add(userB);
+                ctx.Users.Add(userC);
+
+                ctx.SaveChanges();
+
+                var requestB = new UserFollowRequest
+                                   {
+                                       FromUserId = userB.Id,
+                                       ToUserId = userA.Id,
+                                       RequestDate = DateTime.Now
+                                   };
+
+                var requestC = new UserFollowRequest
+                                   {
+                                       FromUserId = userC.Id,
+                                       ToUserId = userA.Id,
+                                       RequestDate = DateTime.Now
+                                   };
+
+                ctx.UserFollowRequests.Add(requestB);
+                ctx.UserFollowRequests.Add(requestC);
+
+                ctx.SaveChanges();
+            }
+
+            //Act
+            var result = _repo.GetReceivedRequestsAsync(userA.Id).Result;
+
+            //Assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [Test]
+        public void GetUserSentFollowRequests_OnGetSentRequestsAsync()
+        {
+            //Arrange (User A sends a request to user B and user C)
+            var userA = Mother.GetUser();
+            var userB = Mother.GetUser();
+            var userC = Mother.GetUser();
+
+            using (var ctx = new ZazzDbContext())
+            {
+                ctx.Users.Add(userA);
+                ctx.Users.Add(userB);
+                ctx.Users.Add(userC);
+
+                ctx.SaveChanges();
+
+                var requestB = new UserFollowRequest
+                {
+                    FromUserId = userA.Id,
+                    ToUserId = userB.Id,
+                    RequestDate = DateTime.Now
+                };
+
+                var requestC = new UserFollowRequest
+                {
+                    FromUserId = userA.Id,
+                    ToUserId = userC.Id,
+                    RequestDate = DateTime.Now
+                };
+
+                ctx.UserFollowRequests.Add(requestB);
+                ctx.UserFollowRequests.Add(requestC);
+
+                ctx.SaveChanges();
+            }
+
+            //Act
+            var result = _repo.GetSentRequestsAsync(userA.Id).Result;
+
+            //Assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+
     }
 }
