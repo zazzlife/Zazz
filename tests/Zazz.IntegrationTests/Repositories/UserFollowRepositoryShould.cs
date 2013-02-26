fd5fdf169b61a4c2faa4aaf168e5d0d3cdc362de
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 16:56:28 +0330

    added user follow repo test class.

---
+++ tests/Zazz.IntegrationTests/Repositories/UserFollowRepositoryShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Zazz.Data;
+using Zazz.Data.Repositories;
+
+namespace Zazz.IntegrationTests.Repositories
+{
+    [TestFixture]
+    public class UserFollowRepositoryShould
+    {
+        private ZazzDbContext _context;
+        private UserFollowRepository _repo;
+
+        [SetUp]
+        public void Init()
+        {
+            _context = new ZazzDbContext(true);
+            _repo = new UserFollowRepository(_context);
+        }
+
+
+    }
+}
