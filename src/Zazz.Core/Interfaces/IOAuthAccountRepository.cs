3b7e26f22c3526cb0d4b05d731ca9bc1c83217ba
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 13:22:25 +0330

    added OAuthAccount repo interface.

---
+++ src/Zazz.Core/Interfaces/IOAuthAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IOAuthAccountRepository
+    {
+        Task<OAuthAccount> GetUserAccount(int userId, OAuthProvider provider);
+
+        Task<IEnumerable<OAuthAccount>> GetUserAccounts(int userId);
+
+        void Remove(int userId, OAuthProvider provider);
+    }
+}
