990dd90cc728996d3c31d8e423df18c165aa5acc
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-26 12:08:08 +0330

    added validation token repo.

---
+++ src/Zazz.Data/Repositories/ValidationTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class ValidationTokenRepository : BaseRepository<ValidationToken>
+    {
+        public ValidationTokenRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+
+        protected override int GetItemId(ValidationToken item)
+        {
+            throw new InvalidOperationException("You should always provide the id for updating the token, if it's new then use insert graph.");
+        }
+    }
+}
