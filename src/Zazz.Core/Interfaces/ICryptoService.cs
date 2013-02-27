85e0720a156faba8724c0adee77bc8120e102a2f
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-27 23:38:15 +0330

    added crypto service and moved the key there.

---
+++ src/Zazz.Core/Interfaces/ICryptoService.cs
@@ -0,0 +1,7 @@
+﻿namespace Zazz.Core.Interfaces
+{
+    public interface ICryptoService
+    {
+        string GeneratePasswordHash(string password);
+    }
+}
---
+++ src/Zazz.Infrastructure/Services/CryptoService.cs
@@ -0,0 +1,19 @@
+﻿using Zazz.Core.Interfaces;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class CryptoService : ICryptoService
+    {
+        
+        /// <summary>
+        /// This key should be used to sign passwords in DB, using SHA1 (HMACSHA1)
+        /// </summary>
+        private static byte[] _passwordHashSecret = { 4, 166, 67, 133, 113, 2, 184, 158 };
+        //Base64: BKZDhXECuJ4=
+
+        public string GeneratePasswordHash(string password)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
