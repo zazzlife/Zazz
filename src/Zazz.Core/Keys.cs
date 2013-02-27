04129c65d2331916e9987404885042225ff13b59
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-27 23:27:53 +0330

    added password hash secret.

---
+++ src/Zazz.Core/Keys.cs
@@ -0,0 +1,12 @@
+﻿namespace Zazz.Core
+{
+    public sealed class Keys
+    {
+        //Base64: BKZDhXECuJ4=
+
+        /// <summary>
+        /// This key should be used to sign passwords in DB, using SHA1 (HMACSHA1)
+        /// </summary>
+        public static byte[] PasswordHashSecret = { 4, 166, 67, 133, 113, 2, 184, 158 }; 
+    }
+}
