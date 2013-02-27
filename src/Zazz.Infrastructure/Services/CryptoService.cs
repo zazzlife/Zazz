ddb2f184ef2ca3535b739ef0239c456c2795ecdf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-27 23:55:26 +0330

    implemented crypto service + tests

--- src/Zazz.Infrastructure/Services/CryptoService.cs
+++ src/Zazz.Infrastructure/Services/CryptoService.cs
@@ -1,19 +1,29 @@
-﻿using Zazz.Core.Interfaces;
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Zazz.Core.Interfaces;
 
 namespace Zazz.Infrastructure.Services
 {
     public class CryptoService : ICryptoService
     {
-        
         /// <summary>
         /// This key should be used to sign passwords in DB, using SHA1 (HMACSHA1)
         /// </summary>
-        private static byte[] _passwordHashSecret = { 4, 166, 67, 133, 113, 2, 184, 158 };
+        private static readonly byte[] PasswordHashSecret = { 4, 166, 67, 133, 113, 2, 184, 158 }; 
         //Base64: BKZDhXECuJ4=
 
         public string GeneratePasswordHash(string password)
         {
-            throw new System.NotImplementedException();
+            if (String.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password");
+
+            var hmacsha1 = new HMACSHA1(PasswordHashSecret);
+            var passBytes = Encoding.UTF8.GetBytes(password);
+
+            var hash = hmacsha1.ComputeHash(passBytes);
+
+            return Convert.ToBase64String(hash);
         }
     }
 }
---
+++ tests/Zazz.UnitTests/Infrastructure/Services/CryptoServiceShould.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using NUnit.Framework;
+using Zazz.Infrastructure.Services;
+
+namespace Zazz.UnitTests.Infrastructure.Services
+{
+    [TestFixture]
+    public class CryptoServiceShould
+    {
+        private CryptoService _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new CryptoService();
+        }
+
+        [Test]
+        public void AlwaysGenerate28Characters()
+        {
+            //Arrange & Act & Assert
+            var randomBytes = new RNGCryptoServiceProvider();
+            var rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var randomData = new byte[rnd.Next(20, 500)];
+                randomBytes.GetBytes(randomData);
+
+                var text = Encoding.UTF8.GetString(randomData);
+                var hash = _sut.GeneratePasswordHash(text);
+
+                Assert.AreEqual(28, hash.Length);
+            }
+        }
+
+        [Test]
+        public void ThrowExceptionWhenPasswordIsNullOrEmpty([Values(null, "")] string pass)
+        {
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => _sut.GeneratePasswordHash(pass));
+        }
+
+        [TestCase("Soroush", "foEQUgWMeaIb+sZ7wnq+rzaTKSQ=")]
+        [TestCase("Test", "bFckl+N4Cyawjh5qBhuSZwTIhRc=")]
+        [TestCase(" ", "obfrqmcoMlblAhU1U6MbbSPg77k=")]
+        [TestCase("TEST", "YkS9cEtjYIOThH24GfMl46q4jD8=")]
+        public void GenerateExpectedValues(string text, string expected)
+        {
+            //Act
+            var hash = _sut.GeneratePasswordHash(text);
+
+            //Assert
+            Assert.AreEqual(expected, hash);
+        }
+    }
+}
