6554e4335e79bef41c89fb575ee1b78567da9b84
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-28 13:16:01 +0330

    added auth service interface.

---
+++ src/Zazz.Core/Interfaces/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IAuthService
+    {
+        Task LoginAsync(string username, string password);
+
+        Task RegisterAsync(User user);
+
+        Task GenerateResetPasswordTokenAsync(string email);
+
+        Task<bool> IsTokenValidAsync(int userId, int token, string tokenHash);
+
+        Task ResetPasswordAsync(int userId, int token, string tokenHash, string newPassword);
+        
+        Task ChangePasswordAsync(int userId, string currentPassword, string newPassword);
+    }
+}
