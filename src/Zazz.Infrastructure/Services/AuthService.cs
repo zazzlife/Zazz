0dde9787b2608bf1e2fd1f1f3d1dca187f6cf34d
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-02-28 13:25:59 +0330

    added empty auth service.

---
+++ src/Zazz.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUoW _uoW;
+
+        public AuthService(IUoW uoW)
+        {
+            _uoW = uoW;
+        }
+
+        public Task LoginAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GenerateResetPasswordTokenAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsTokenValidAsync(int userId, Guid token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ResetPasswordAsync(int userId, Guid token, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangePasswordAsync(int userId, string currentPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
