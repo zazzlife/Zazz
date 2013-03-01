486694eb9d0025a180b68301b2a8ccaeae8d8b5d
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-01 13:53:48 +0330

    added more secret keys.

--- src/Zazz.Infrastructure/Services/CryptoService.cs
+++ src/Zazz.Infrastructure/Services/CryptoService.cs
@@ -10,8 +10,18 @@ namespace Zazz.Infrastructure.Services
         /// <summary>
         /// This key should be used to sign passwords in DB, using SHA1 (HMACSHA1)
         /// </summary>
-        private static readonly byte[] PasswordHashSecret = { 4, 166, 67, 133, 113, 2, 184, 158 }; 
+        private static readonly byte[] PasswordHashSecret = { 4, 166, 67, 133, 113, 2, 184, 158 }; //64 bit
         //Base64: BKZDhXECuJ4=
+
+        /// <summary>
+        /// This key should be used to sign a string to make sure it's not tampered with later.
+        /// </summary>
+        private static readonly byte[] RandomSignHashSecret =
+            {
+                109, 17, 99, 216, 57, 55, 193, 244, 152, 214, 126, 37,
+                177, 169, 58, 211
+            }; //128 bit
+        //Base64: bRFj2Dk3wfSY1n4lsak60w==
 
         public string GeneratePasswordHash(string password)
         {
@@ -25,5 +35,18 @@ namespace Zazz.Infrastructure.Services
 
             return Convert.ToBase64String(hash);
         }
+
+
+
+        /*KEYS FOR LATER USE
+         
+         * 128 bit
+         * Wmk9XcunbkZerlTkIPX8ng==
+         * {90, 105, 61, 93, 203, 167, 110, 70, 94, 174, 84, 228, 32, 245, 252, 158}
+         
+         * 64 bit
+         * eQCCZ7vEAwE=
+         * {121, 0, 130, 103, 187, 196, 3, 1}
+         */
     }
 }
