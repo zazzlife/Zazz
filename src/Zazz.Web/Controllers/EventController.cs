1315fac9aeb7d8d4b61c8fb2c43dc51510b256f6
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-02 12:24:02 +0330

    added event controller

---
+++ src/Zazz.Web/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zazz.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
