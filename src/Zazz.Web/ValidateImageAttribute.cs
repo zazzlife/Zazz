1eb4ea368a72ed03889800e5aaccdec584d29e2e
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-06 20:40:08 +0330

    added image validation attribute

---
+++ src/Zazz.Web/ValidateImageAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Web;
+
+namespace Zazz.Web
+{
+    public class ValidateImageAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var file = value as HttpPostedFileBase;
+            if (file == null)
+            {
+                return false;
+            }
+
+            if (file.ContentLength > 2 * 1024 * 1024)
+            {
+                return false;
+            }
+
+            try
+            {
+                using (var img = Image.FromStream(file.InputStream))
+                {
+                    //return img.RawFormat.Equals(ImageFormat.Png);
+                    return true;
+                }
+            }
+            catch { }
+            return false;
+        }
+    }
+}
