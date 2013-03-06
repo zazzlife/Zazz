388d9941a3d27851bed597eb9b92d7d3cccbbddf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-06 15:03:44 +0330

    added user event service

---
+++ src/Zazz.Core/Interfaces/IUserEventService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IUserEventService
+    {
+        Task CreateEventAsync(UserEvent userEvent);
+
+        /// <summary>
+        /// Updates an event.
+        /// </summary>
+        /// <param name="userEvent">The event to update. (Id is required)</param>
+        /// <param name="currentUserId">UserId of the current user. (Used for security check)</param>
+        /// <returns></returns>
+        Task UpdateEventAsync(UserEvent userEvent, int currentUserId);
+
+        /// <summary>
+        /// Deletes an event
+        /// </summary>
+        /// <param name="userEventId">Id of the event to delete.</param>
+        /// <param name="currentUserId">UserId of the current user. (Used for security check)</param>
+        /// <returns></returns>
+        Task DeleteEventAsync(int userEventId, int currentUserId);
+    }
+}
---
+++ src/Zazz.Infrastructure/Services/UserEventService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class UserEventService : IUserEventService
+    {
+        private readonly IUoW _uow;
+
+        public UserEventService(IUoW uow)
+        {
+            _uow = uow;
+        }
+
+        public Task CreateEventAsync(UserEvent userEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateEventAsync(UserEvent userEvent, int currentUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteEventAsync(int userEventId, int currentUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
