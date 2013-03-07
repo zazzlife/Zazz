1a027e4532bbbc890677d449443cbc662bb4b825
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-07 15:37:32 +0330

    added get photo description to photo repo

--- src/Zazz.Data/Repositories/PhotoRepository.cs
+++ src/Zazz.Data/Repositories/PhotoRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
 using Zazz.Core.Interfaces;
 using Zazz.Core.Models.Data;
 
@@ -14,6 +16,13 @@ namespace Zazz.Data.Repositories
         protected override int GetItemId(Photo item)
         {
             throw new InvalidOperationException("You should always provide the id for updating the image, if it's new then use insert graph.");
+        }
+
+        public Task<string> GetDescriptionAsync(int photoId)
+        {
+            return Task.Run(() => DbSet.Where(p => p.Id == photoId)
+                                       .Select(p => p.Description)
+                                       .SingleOrDefault());
         }
     }
 }
---
+++ tests/Zazz.IntegrationTests/Repositories/PhotoRepositoryShould.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Zazz.Core.Models.Data;
+using Zazz.Data;
+using Zazz.Data.Repositories;
+
+namespace Zazz.IntegrationTests.Repositories
+{
+    [TestFixture]
+    public class PhotoRepositoryShould
+    {
+        private ZazzDbContext _dbContext;
+        private PhotoRepository _repo;
+
+        [SetUp]
+        public void Init()
+        {
+            _dbContext = new ZazzDbContext(true);
+            _repo = new PhotoRepository(_dbContext);
+        }
+
+        [Test]
+        public async Task ReturnDescription_OnGetDescription()
+        {
+            //Arrange
+            var description = "test";
+            var user = Mother.GetUser();
+            var album = new Album {Name = "name"};
+            var photo = new Photo {Description = description, UploadDate = DateTime.UtcNow};
+
+            using (var ctx = new ZazzDbContext())
+            {
+                ctx.Users.Add(user);
+                ctx.SaveChanges();
+
+                album.UserId = user.Id;
+                photo.UploaderId = user.Id;
+
+                ctx.Albums.Add(album);
+                ctx.SaveChanges();
+
+                photo.AlbumId = album.Id;
+                ctx.Photos.Add(photo);
+                ctx.SaveChanges();
+            }
+
+            //Act
+            var result = await _repo.GetDescriptionAsync(photo.Id);
+
+            //Assert
+            Assert.AreEqual(description, result);
+
+        }
+
+
+    }
+}
