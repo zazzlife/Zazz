77a0664605ecdc1d7375ed72e1c0491224c0d79c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-08 14:23:24 +0330

    added facebook photo and image models

---
+++ src/Zazz.Core/Models/Facebook/FbImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Zazz.Core.Models.Facebook
+{
+    [DataContract]
+    public class FbImage
+    {
+        /// <summary>
+        /// Url to the original image.
+        /// </summary>
+        [DataMember(Name = "source")]
+        public string Source { get; set; }
+
+        [DataMember(Name = "height")]
+        public int Height { get; set; }
+
+        [DataMember(Name = "width")]
+        public int Width { get; set; }
+    }
+}
---
+++ src/Zazz.Core/Models/Facebook/FbPhoto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Zazz.Core.Models.Facebook
+{
+    [DataContract]
+    public class FbPhoto : FbImage
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "from")]
+        public FbUser From { get; set; }
+
+        [DataMember(Name = "images")]
+        public IEnumerable<FbImage> Images { get; set; }
+
+        [DataMember(Name = "created_time")]
+        public DateTime CreatedTime { get; set; }
+    }
+}
