e33909fc5dfa61308ed03c204a1c02bd0f4dba85
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-09 16:11:06 +0330

    catching exceptions on fake email service

--- src/Zazz.Infrastructure/FakeEMailService.cs
+++ src/Zazz.Infrastructure/FakeEMailService.cs
@@ -1,18 +1,24 @@
-﻿using System.Net.Mail;
+﻿using System;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Zazz.Core.Interfaces;
 
 namespace Zazz.Infrastructure
 {
     /// <summary>
-    /// This class sends an email to localhost smtp server. Use a fake smtp server and only use for debugging.
+    /// This class sends an email to localhost smtp server. Will ignore erros if smtp is not present. Only use for debugging.
     /// </summary>
     public class FakeEmailService : IEmailService
     {
         public async Task SendAsync(MailMessage message)
         {
-            using (var smtp = new SmtpClient("localhost"))
-                await smtp.SendMailAsync(message);
+            try
+            {
+                using (var smtp = new SmtpClient("localhost"))
+                    await smtp.SendMailAsync(message);
+            }
+            catch (Exception)
+            {}
         }
     }
 }
