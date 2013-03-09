5cd5974983b7dddb47e1c68fbca9f4acc283c22c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-09 14:20:17 +0330

    implemented logger

--- src/Zazz.Core/Interfaces/ILogger.cs
+++ src/Zazz.Core/Interfaces/ILogger.cs
@@ -1,11 +1,14 @@
-﻿namespace Zazz.Core.Interfaces
+﻿using System;
+
+namespace Zazz.Core.Interfaces
 {
     public interface ILogger
     {
-        void LogDebug(string nameSpace, string message);
-        void LogFatal(string nameSpace, string message);
+        void LogFatal(string nameSpace, string message, Exception exception);
         void LogError(string nameSpace, string message);
         void LogWarning(string nameSpace, string message);
         void LogInfo(string nameSpace, string message);
+        void LogDebug(string nameSpace, string message);
+
     }
 }
---
+++ src/Zazz.Infrastructure/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Zazz.Core.Interfaces;
+using log4net;
+
+namespace Zazz.Infrastructure
+{
+    public class Logger : ILogger
+    {
+        public void LogFatal(string nameSpace, string message, Exception exception)
+        {
+            var logger = LogManager.GetLogger(nameSpace);
+            if (logger.IsFatalEnabled)
+                logger.Fatal(message, exception);
+        }
+
+        public void LogError(string nameSpace, string message)
+        {
+            var logger = LogManager.GetLogger(nameSpace);
+            if (logger.IsErrorEnabled)
+                logger.Error(message);
+        }
+
+        public void LogWarning(string nameSpace, string message)
+        {
+            var logger = LogManager.GetLogger(nameSpace);
+            if (logger.IsWarnEnabled)
+                logger.Warn(message);
+        }
+
+        public void LogInfo(string nameSpace, string message)
+        {
+            var logger = LogManager.GetLogger(nameSpace);
+            if (logger.IsInfoEnabled)
+                logger.Info(message);
+        }
+
+        public void LogDebug(string nameSpace, string message)
+        {
+            var logger = LogManager.GetLogger(nameSpace);
+            if (logger.IsDebugEnabled)
+                logger.Debug(message);
+        }
+    }
+}
