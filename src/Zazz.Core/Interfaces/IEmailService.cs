5ffd724177e5d34bf3414fac5b99b0108efc4fdf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-09 16:07:40 +0330

    added fake mail service

---
+++ src/Zazz.Core/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendAsync(MailMessage message);
+    }
+}
---
+++ src/Zazz.Infrastructure/FakeEMailService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.Infrastructure
+{
+    /// <summary>
+    /// This class sends an email to localhost smtp server. Use a fake smtp server and only use for debugging.
+    /// </summary>
+    public class FakeEmailService : IEmailService
+    {
+        public async Task SendAsync(MailMessage message)
+        {
+            using (var smtp = new SmtpClient("localhost"))
+                await smtp.SendMailAsync(message);
+        }
+    }
+}
