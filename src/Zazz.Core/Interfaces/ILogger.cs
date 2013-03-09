b8b6921da57f90881a130e93a902674bbde451c3
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-09 12:43:44 +0330

    added logger interface

---
+++ src/Zazz.Core/Interfaces/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Zazz.Core.Interfaces
+{
+    public interface ILogger
+    {
+        void LogDebug(string nameSpace, string message);
+        void LogFatal(string nameSpace, string message);
+        void LogError(string nameSpace, string message);
+        void LogWarning(string nameSpace, string message);
+        void LogInfo(string nameSpace, string message);
+    }
+}
