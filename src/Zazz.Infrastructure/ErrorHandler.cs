65a6551c1ce570a8d274f3521befae1d1dca43a1
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-09 16:21:53 +0330

    added error handler

---
+++ src/Zazz.Infrastructure/ErrorHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.Infrastructure
+{
+    public class ErrorHandler : IErrorHandler
+    {
+        private readonly IUoW _uoW;
+        private readonly ILogger _logger;
+        private readonly IEmailService _emailService;
+
+        public ErrorHandler(IUoW uoW, ILogger logger, IEmailService emailService)
+        {
+            _uoW = uoW;
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+        public Task HandleAccessTokenExpiredAsync(string fbUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleFacebookApiLimitAsync(string fbUserId, string path, string fields)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleFacebookApiErrorAsync(string fbUserId, string methodName, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Infrastructure/ErrorHandlerShould.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Zazz.Core.Interfaces;
+using Zazz.Infrastructure;
+
+namespace Zazz.UnitTests.Infrastructure
+{
+    [TestFixture]
+    public class ErrorHandlerShould
+    {
+        private Mock<IUoW> _uow;
+        private Mock<ILogger> _logger;
+        private Mock<IEmailService> _emailService;
+        private ErrorHandler _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _uow = new Mock<IUoW>();
+            _logger = new Mock<ILogger>();
+            _emailService = new Mock<IEmailService>();
+            _sut = new ErrorHandler(_uow.Object, _logger.Object, _emailService.Object);
+
+            _uow.Setup(x => x.SaveAsync())
+                .Returns(() => Task.Run(() => { }));
+        }
+    }
+}
