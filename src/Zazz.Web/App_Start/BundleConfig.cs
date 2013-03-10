4c329d81dfd60d2525567ff8a96dfc0e4b180262
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-10 16:07:04 +0330

    loading jquery ui from local for now

--- src/Zazz.Web/App_Start/BundleConfig.cs
+++ src/Zazz.Web/App_Start/BundleConfig.cs
@@ -14,7 +14,18 @@ namespace Zazz.Web
             var js = new ScriptBundle("~/js");
             js.IncludeDirectory("~/Scripts/", "*.js", true);
 
+            var jqueryUiCss = new StyleBundle("~/Content/themes/base/css",
+                                              "http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css")
+                .Include(
+                    "~/Content/themes/base/jquery.ui.core.css",
+                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+                    "~/Content/themes/base/jquery.ui.selectable.css",
+                    "~/Content/themes/base/jquery.ui.slider.css",
+                    "~/Content/themes/base/jquery.ui.datepicker.css",
+                    "~/Content/themes/base/jquery.ui.theme.css");
+
             bundles.Add(css);
+            bundles.Add(jqueryUiCss);
             bundles.Add(js);
         }
     }
