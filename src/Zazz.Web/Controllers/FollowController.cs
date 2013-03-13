799f61255efb449959f145e8a35dd960f86b6717
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-13 16:49:11 +0330

    addded follow controller

---
+++ src/Zazz.Web/Controllers/FollowController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zazz.Web.Controllers
+{
+    [Authorize]
+    public class FollowController : BaseController
+    {
+        public void FollowUser(int id)
+        {
+            
+        }
+
+        public void AcceptFollow(int id)
+        {
+            
+        }
+
+        public void RejectFollow(int id)
+        {
+            
+        }
+
+        public ActionResult GetFollowRequests()
+        {
+            return View();
+        }
+    }
+}
