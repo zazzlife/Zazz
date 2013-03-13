7fa9e7ef341a4d51d4e194420a1fb82db9bda0aa
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-13 19:51:13 +0330

    implemented accept/reject/request follow on controller

--- src/Zazz.Web/Controllers/FollowController.cs
+++ src/Zazz.Web/Controllers/FollowController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
 
 namespace Zazz.Web.Controllers
 {
@@ -11,28 +12,51 @@ namespace Zazz.Web.Controllers
     public class FollowController : BaseController
     {
         private readonly IFollowService _followService;
+        private readonly IUserService _userService;
 
-        public FollowController(IFollowService followService)
+        public FollowController(IFollowService followService, IUserService userService)
         {
             _followService = followService;
+            _userService = userService;
         }
 
-        public void FollowUser(int id)
+        public async void FollowUser(int id)
         {
             using (_followService)
+            using (_userService)
             {
-                
+                var currentUserId = _userService.GetUserId(User.Identity.Name);
+                var accountType = _userService.GetUserAccountType(id);
+
+                if (accountType == AccountType.User)
+                {
+                    await _followService.SendFollowRequestAsync(currentUserId, id);
+                }
+                else if (accountType == AccountType.ClubAdmin)
+                {
+                    await _followService.FollowClubAdminAsync(currentUserId, id);
+                }
             }
         }
 
-        public void AcceptFollow(int id)
+        public async void AcceptFollow(int id)
         {
-            
+            using (_followService)
+            using (_userService)
+            {
+                var currentUserId = _userService.GetUserId(User.Identity.Name);
+                await _followService.AcceptFollowRequestAsync(id, currentUserId);
+            }
         }
 
-        public void RejectFollow(int id)
+        public async void RejectFollow(int id)
         {
-            
+            using (_followService)
+            using (_userService)
+            {
+                var currentUserId = _userService.GetUserId(User.Identity.Name);
+                await _followService.RejectFollowRequestAsync(id, currentUserId);
+            }
         }
 
         public ActionResult GetFollowRequests()
