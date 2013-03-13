1b3f72f7a5668e9f10a6e597e93e8fa41e355914
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-13 18:40:05 +0330

    implemented accept/reject follow request script

--- src/Zazz.Web/Scripts/app/global.js
+++ src/Zazz.Web/Scripts/app/global.js
@@ -1,7 +1,7 @@
 ﻿function showBtnBusy(btn) {
     var originalText = $(btn).html();
 
-    var textWithSpinner = '<i class="icon-refresh icon-spin"></i>' + originalText;
+    var textWithSpinner = '<i style="margin-right:5px;" class="icon-refresh icon-spin"></i>' + originalText;
 
     $(btn).attr('disabled', 'disabled');
     $(btn).addClass('disabled');
@@ -10,10 +10,48 @@
     return originalText;
 }
 
+function hideBtnBusy(btn, originalText) {
+    $(btn).removeAttr('disabled', 'disabled');
+    $(btn).removeClass('disabled');
+    $(btn).html(originalText);
+}
+
 $(function () {
     
     $('.datepicker').datetimepicker();
 
-
     $('*[title]').tooltip();
+
+
+    $('.btn-followrequest-action').click(function () {
+        var btn = $(this);
+        var originalBtnText = showBtnBusy(btn);
+
+        var url = btn.data('url');
+
+        var options = {
+            url: url,
+            cache: false,
+            error: function() {
+                toastr.error("An error occured, Please try again later.");
+            }
+        };
+
+        $.ajax(options).done(function () {
+            btn.closest('tr').fadeOut('fast');
+
+            var followReqCountElem = $('#follow-request-count');
+            var followReqCount = parseInt(followReqCountElem.text());
+
+            followReqCount--;
+
+            followReqCountElem.text(followReqCount);
+
+            if (followReqCount < 1) {
+                followReqCountElem.fadeOut();
+            }
+                
+        });
+
+    });
 })
