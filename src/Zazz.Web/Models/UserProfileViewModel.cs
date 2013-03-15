616c0c0119061ac8676d25e879ae51aab1e40103
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-15 11:08:23 +0330

    added user profile vm

---
+++ src/Zazz.Web/Models/UserProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Zazz.Web.Models
+{
+    public class UserProfileViewModel
+    {
+        public string CoverPhotoUrl { get; set; }
+
+        public string UserPhotoUrl { get; set; }
+
+        public string Major { get; set; }
+
+        public string University { get; set; }
+
+        public string City { get; set; }
+
+        public string UserName { get; set; }
+
+        public int PartyWebCount { get; set; }
+
+        public bool IsSelf { get; set; }
+
+        public bool IsTargetUserFollowingCurrentUser { get; set; }
+
+        public bool IsCurrentUserFollowingTargetUser { get; set; }
+    }
+
+}
