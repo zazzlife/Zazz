c781e8c3064ac2e10af7f4bff2e1e3a333a115d5
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-17 13:53:48 +0430

    some modifications to feeds entity

--- src/Zazz.Core/Models/Data/Feed.cs
+++ src/Zazz.Core/Models/Data/Feed.cs
@@ -1,18 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Zazz.Core.Models.Data
 {
     public class Feed : BaseEntity
     {
         [ForeignKey("UserId")]
-        public User User { get; set; }
+        public virtual User User { get; set; }
 
         public int? UserId { get; set; }
 
         [ForeignKey("PostId")]
-        public Post Post { get; set; }
+        public virtual Post Post { get; set; }
 
         public int? PostId { get; set; }
+
+        [ForeignKey("PhotoId")]
+        public virtual Photo Photo { get; set; }
+
+        public int? PhotoId { get; set; }
+
+        public DateTime Time { get; set; }
 
         public FeedType FeedType { get; set; }
     }
