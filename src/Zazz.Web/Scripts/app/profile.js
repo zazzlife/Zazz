f7fa07fac3c516f8d8df38d63ad5144b0cf524d0
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-21 23:25:44 +0430

    added stop follow script

--- src/Zazz.Web/Scripts/app/profile.js
+++ src/Zazz.Web/Scripts/app/profile.js
@@ -1,15 +1,51 @@
 ﻿
+var isFollowSignClicked = false;
 $('#followsYouSign').hover(
     function mouseIn() {
 
-        $(this).removeClass('label-success');
-        $(this).addClass('label-warning');
-        $(this).html('Stop follow  <i class="icon icon-remove-sign"></i>');
+        if (isFollowSignClicked) {
+            return;
+        }
+
+        var sign = $(this);
+
+        sign.removeClass('label-success');
+        sign.addClass('label-warning');
+        sign.html('Stop follow  <i class="icon icon-remove-sign"></i>');
 
     }, function mouseOut() {
 
-        $(this).addClass('label-success');
-        $(this).removeClass('label-warning');
-        $(this).html('Follows you <i class="icon icon-ok-sign"></i>');
+        if (isFollowSignClicked) {
+            return;
+        }
+
+        var sign = $(this);
+
+        sign.addClass('label-success');
+        sign.removeClass('label-warning');
+        sign.html('Follows you <i class="icon icon-ok-sign"></i>');
         
     });
+
+$('#followsYouSign').click(function(e) {
+    var sign = $(this);
+
+    var userId = sign.data('id');
+
+    isFollowSignClicked = true;
+    sign.removeAttr('id');
+    sign.css('opacity', 0.5);
+    sign.html('Working <i class="icon icon-spinner icon-spin"></i>');
+
+    $.ajax({
+        url: '/follow/stopfollow/' + userId,
+        cache: false,
+        error: function() {
+            toastr.error('An error occured. Please try again later.');
+        },
+        success: function() {
+            toastr.success('This user is no longer following you!');
+            sign.fadeOut();
+        }
+    });
+});
