e1fbb50cde84c61b3de674f6a92e7f01afc9d891
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-21 23:09:29 +0430

    working on follow sign script

---
+++ src/Zazz.Web/Scripts/app/profile.js
@@ -0,0 +1,15 @@
+﻿
+$('#followsYouSign').hover(
+    function mouseIn() {
+
+        $(this).removeClass('label-success');
+        $(this).addClass('label-warning');
+        $(this).html('Stop follow  <i class="icon icon-remove-sign"></i>');
+
+    }, function mouseOut() {
+
+        $(this).addClass('label-success');
+        $(this).removeClass('label-warning');
+        $(this).html('Follows you <i class="icon icon-ok-sign"></i>');
+        
+    });
