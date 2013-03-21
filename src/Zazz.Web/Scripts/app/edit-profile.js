76daa494cd85b9426394f93dd10dc9caa9e4b4c1
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-21 11:56:57 +0430

    refactored img crop script a little

--- src/Zazz.Web/Scripts/app/edit-profile.js
+++ src/Zazz.Web/Scripts/app/edit-profile.js
@@ -17,27 +17,35 @@ $('#pg-modalWithCrop').on('show', function() {
 
 });
 
-$(document).on('click', '#pg-modalWithCrop button[data-selectPhoto]', function (e) {
+function showCropImg(modalBody, imgId, imgUrl) {
 
-    e.preventDefault();
-
-    cropPhotoId = $(this).data('id');
-    var imgUrl = $(this).data('url');
-
+    cropPhotoId = imgId;
+    
     var html = '<img id="cropImg" src="' + imgUrl + '" />';
     html += '<button style="margin-top:10px;" id="cropBtn" type="button" class="btn btn-info">Crop</button>';
 
-    var modalBody = $(this).closest('.modal-body');
-    modalBody.slideUp(function() {
+    modalBody.slideUp(function () {
         modalBody.html(html);
         modalBody.slideDown();
 
         $('#cropImg').Jcrop({
             boxWidth: 530,
             onChange: onCropSelectionChange,
-            aspectRatio: 10/3
+            aspectRatio: 10 / 3
         });
     });
+
+}
+
+$(document).on('click', '#pg-modalWithCrop button[data-selectPhoto]', function (e) {
+
+    e.preventDefault();
+
+    var imgId = $(this).data('id');
+    var imgUrl = $(this).data('url');
+    var modalBody = $(this).closest('.modal-body');
+
+    showCropImg(modalBody, imgId, imgUrl);
 });
 
 $(document).on('click', '#cropBtn', function(e) {
