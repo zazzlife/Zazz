395726e056e7284806782fefba18a4b35d31ee6b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-21 15:02:46 +0430

    implemented search

--- src/Zazz.Web/Controllers/HomeController.cs
+++ src/Zazz.Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Zazz.Core.Interfaces;
 using Zazz.Core.Models.Data;
+using Zazz.Infrastructure;
 using Zazz.Web.Helpers;
 using Zazz.Web.Models;
 
@@ -44,18 +45,43 @@ namespace Zazz.Web.Controllers
 
         public JsonNetResult Search(string q)
         {
-            var response = new List<AutocompleteResponse>();
-            for (int i = 0; i < 5; i++)
+            using (_uow)
+            using (_photoService)
+            using (_userService)
             {
-                response.Add(new AutocompleteResponse
-                             {
-                                 Id = i,
-                                 Img = "/picture/user/1/8/38.jpg",
-                                 Value = "User " + i
-                             });
-            }
+                var users = _uow.UserRepository.GetAll()
+                                .Where(u =>
+                                    u.UserDetail.FullName.Contains(q) || u.Username.Contains(q) ||
+                                    u.ClubDetail.ClubName.Contains(q))
+                                .Select(u => new
+                                             {
+                                                 id = u.Id,
+                                                 username = u.Username,
+                                                 fullname = u.UserDetail.FullName,
+                                                 gender = u.UserDetail.Gender,
+                                                 picId = u.UserDetail.ProfilePhotoId
+                                             })
+                                .Take(5);
 
-            return new JsonNetResult { Data = response, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                var response = new List<AutocompleteResponse>();
+
+                foreach (var u in users)
+                {
+                    response.Add(new AutocompleteResponse
+                                 {
+                                     Id = u.id,
+                                     Value = !String.IsNullOrEmpty(u.fullname)
+                                                 ? u.fullname
+                                                 : u.username,
+                                     Img = u.picId == 0
+                                               ? DefaultImageHelper.GetUserDefaultImage(u.gender)
+                                               : _photoService.GeneratePhotoUrl(u.id, u.picId)
+                                 });
+                }
+
+
+                return new JsonNetResult {Data = response, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+            }
         }
 
         public class AutocompleteResponse
