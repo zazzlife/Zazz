29590123c30cfebe5046b7c806cd69d0d903726b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-22 19:17:28 +0430

    added comment view models

---
+++ src/Zazz.Web/Models/CommentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zazz.Web.Models
+{
+    /// <summary>
+    /// View Model of a single comment
+    /// </summary>
+    public class CommentViewModel
+    {
+        public int CommentId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string UserPhotoUrl { get; set; }
+
+        public string UserDisplayName { get; set; }
+
+        public string CommentText { get; set; }
+
+        public bool IsFromCurrentUser { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
---
+++ src/Zazz.Web/Models/CommentsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Web.Models
+{
+    /// <summary>
+    /// View Model of Comments for a certain feed.
+    /// </summary>
+    public class CommentsViewModel
+    {
+        public int UserId { get; set; }
+
+        public string UserPhotoUrl { get; set; }
+
+        public string UserDisplayName { get; set; }
+
+        /// <summary>
+        /// This is id of the item that the user can comment on. (post/event/photo/...)
+        /// </summary>
+        public int ItemId { get; set; }
+
+        public FeedType FeedType { get; set; }
+
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+    }
+}
