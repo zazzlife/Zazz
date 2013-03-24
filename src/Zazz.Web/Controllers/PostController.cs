92c17efb0d38051d5794b5d8119fa02d65e55353
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-24 14:26:00 +0430

    working on post controller

---
+++ src/Zazz.Web/Controllers/PostController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IUoW _uow;
+
+        public PostController(IUoW uow)
+        {
+            _uow = uow;
+        }
+
+        [Authorize, HttpPost]
+        public async Task<ActionResult> New(string message)
+        {
+            using (_uow)
+            {
+                if (String.IsNullOrEmpty(message))
+                    throw new ArgumentNullException("message");
+
+                var userId = _uow.UserRepository.GetIdByUsername(User.Identity.Name);
+                var post = new Post
+                           {
+                               CreatedTime = DateTime.UtcNow,
+                               Message = message,
+                               UserId = userId
+                           };
+
+                return View("FeedItems/_PostFeedItem");
+            }
+        }
+    }
+}
