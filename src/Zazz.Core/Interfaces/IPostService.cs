141f073f9c2973f92afbadf774ee4c9822de405b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-24 14:27:52 +0430

    added post service

---
+++ src/Zazz.Core/Interfaces/IPostService.cs
@@ -0,0 +1,9 @@
+﻿using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IPostService
+    {
+        void NewPost(Post post);
+    }
+}
---
+++ src/Zazz.Infrastructure/Services/PostService.cs
@@ -0,0 +1,20 @@
+﻿using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IUoW _uow;
+
+        public PostService(IUoW uow)
+        {
+            _uow = uow;
+        }
+
+        public void NewPost(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
