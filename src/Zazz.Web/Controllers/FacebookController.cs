63c5b8d11c67e45b9d41663018df2d2af002d4bf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-26 13:28:15 +0430

    implemented facebook real time verification

---
+++ src/Zazz.Web/Controllers/FacebookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web;
+using System.Web.Mvc;
+using Zazz.Infrastructure;
+
+namespace Zazz.Web.Controllers
+{
+    public class FacebookController : Controller
+    {
+        public string Update()
+        {
+            var mode = Request.QueryString["hub.mode"];
+
+            if (!String.IsNullOrEmpty(mode) && mode.Equals("subscribe", StringComparison.InvariantCultureIgnoreCase))
+                return VerifySubscription();
+
+
+            return String.Empty;
+        }
+
+        private string VerifySubscription()
+        {
+            var challenge = Request.QueryString["hub.challenge"];
+            var verifyToken = Request.QueryString["hub.verify_token"];
+
+            if (verifyToken != ApiKeys.FACEBOOK_REALTIME_VERIFY_TOKEN)
+                throw new SecurityException();
+
+            return challenge;
+        }
+    }
+}
