74006f0154e0e8e88b079a73c7c4addeba73811e
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-26 22:11:08 +0430

    working on getting events from facebook

--- src/Zazz.Core/Interfaces/IFacebookHelper.cs
+++ src/Zazz.Core/Interfaces/IFacebookHelper.cs
@@ -1,10 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Zazz.Core.Models.Facebook;
 
 namespace Zazz.Core.Interfaces
 {
     public interface IFacebookHelper
     {
         void SetAccessToken(string token);
+
+        Task<IEnumerable<FbEvent>> GetEvents(int creatorId, long fromDate = 0);
 
         Task<T> GetAsync<T>(string path) where T : class;
 
--- src/Zazz.Infrastructure/FacebookHelper.cs
+++ src/Zazz.Infrastructure/FacebookHelper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Facebook;
 using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Facebook;
 
 namespace Zazz.Infrastructure
 {
@@ -14,9 +16,28 @@ namespace Zazz.Infrastructure
             _client = new FacebookClient();
         }
 
+        private string GenerateFql(string fields, string table, string where)
+        {
+            return String.Format("SELECT {0} FROM {1} WHERE {2}", fields, table, where);
+        }
+
         public void SetAccessToken(string token)
         {
             _client.AccessToken = token;
+        }
+
+        public Task<IEnumerable<FbEvent>> GetEvents(int creatorId, long fromDate = 0)
+        {
+            const string FIELDS = "description, eid, name, location, pic_square, start_time, end_time, update_time, venue";
+            const string TABLE = "event";
+
+            if (fromDate == 0)
+                fromDate = DateTime.UtcNow.ToUnixTimestamp();
+
+            var where = String.Format("creator = {0} AND start_time > {1}", creatorId, fromDate);
+            var query = GenerateFql(FIELDS, TABLE, where);
+
+            return _client.GetTaskAsync<IEnumerable<FbEvent>>("fql", new { q = query });
         }
 
         public Task<T> GetAsync<T>(string path) where T : class
