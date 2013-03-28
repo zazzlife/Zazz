138fbffc2b365c84f336f29dd5c96d68c5899c35
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-03-28 16:59:58 +0430

    some fixes

--- src/Zazz.Web/Scripts/app/user-home-page.js
+++ src/Zazz.Web/Scripts/app/user-home-page.js
@@ -17,4 +17,30 @@
         }
     });
 
-})
+});
+
+$(document).on('click', '#linkPageBtn', function () {
+
+    var self = $(this);
+    var url = '/facebook/linkpage';
+    var pageId = self.data('id');
+
+    showBtnBusy(self);
+
+    $.ajax({
+        url: url,
+        data: {
+            pageId: pageId
+        },
+        error: function() {
+            toastr.error('An error occured, Please try again later.');
+            hideBtnBusy(self, "Link");
+        },
+        success: function() {
+            toastr.success("This page has been successfully linked!");
+            var parent = self.parent();
+            parent.html('<button id="unlinkPageBtn" data-id="@p.Id" class="btn btn-danger">Un-link</button>');
+        }
+    });
+
+});
