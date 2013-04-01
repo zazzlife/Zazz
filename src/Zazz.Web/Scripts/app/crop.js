8384d38ad5ccfa9dbbead66203d53a71b1f2cf62
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-01 18:35:36 +0430

    working on jcrop

---
+++ src/Zazz.Web/Scripts/app/crop.js
@@ -0,0 +1,29 @@
+﻿$('#cropImg').ensureLoad(function () {
+
+    var imgElem = document.getElementById("cropImg");
+    var height = imgElem.naturalHeight;
+    var width = imgElem.naturalWidth;
+    var aspectRatio = $('#aspectRatio').val();
+
+    var x = $('#x');
+    var x2 = $('#x2');
+    var y = $('#y');
+    var y2 = $('#y2');
+    var w = $('#w');
+    var h = $('#h');
+
+    $(imgElem).Jcrop({
+        trueSize: [width, height],
+        aspectRatio: aspectRatio,
+        onChange: function (c) {
+            console.log(c);
+            x.val(c.x);
+            x2.val(c.x2);
+            y.val(c.y);
+            y2.val(c.y2);
+            w.val(c.w);
+            h.val(c.h);
+        }
+    });
+
+});
---
+++ src/Zazz.Web/Scripts/jquery.extends.js
@@ -0,0 +1,12 @@
+﻿http://stackoverflow.com/questions/5624733/jquery-load-not-firing-on-images-probably-caching
+jQuery.fn.extend({
+    ensureLoad: function(handler) {
+        return this.each(function() {
+            if(this.complete) {
+                handler.call(this);
+            } else {
+                $(this).load(handler);
+            }
+        });
+    }
+});
