e8078f917e3851524b6db3f7d1a65fb55ba966fc
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-03 17:49:16 +0430

    working on CB cache 1

---
+++ src/Zazz.Infrastructure/CircularBufferCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Zazz.Infrastructure
+{
+    public class CircularBufferCache<TKey, TVal>
+    {
+        private readonly int _maximumSize;
+        internal ConcurrentDictionary<TKey, TVal> Items;
+        internal ConcurrentDictionary<TKey, int> RequestsCounter;
+
+        public CircularBufferCache(int maximumSize)
+        {
+            _maximumSize = maximumSize;
+            Items = new ConcurrentDictionary<TKey, TVal>();
+            RequestsCounter = new ConcurrentDictionary<TKey, int>();
+        }
+
+        public void Add(TKey key, TVal val)
+        {
+            Items.TryAdd(key, val);
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Infrastructure/CircularBufferCacheShould.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Zazz.Infrastructure;
+
+namespace Zazz.UnitTests.Infrastructure
+{
+    [TestFixture]
+    public class CircularBufferCacheShould
+    {
+        private CircularBufferCache<string, int> _sut;
+        private string _key;
+        private int _val;
+        private const int MAXIMUM_SIZE = 50;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new CircularBufferCache<string, int>(MAXIMUM_SIZE);
+
+            _key = "soroush";
+            _val = 1;
+        }
+
+        [Test]
+        public void ShouldAddToItemsOnAdd()
+        {
+            //Arrange
+            //Act
+            _sut.Add(_key, _val);
+
+            //Assert
+            Assert.IsTrue(_sut.Items.ContainsKey(_key));
+            Assert.AreEqual(_val, _sut.Items[_key]);
+        }
+    }
+}
