efdbcef1734d4f913761880eebaab86b92d69291
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-03 19:00:44 +0430

    implemented cb cache

--- src/Zazz.Infrastructure/CircularBufferCache.cs
+++ src/Zazz.Infrastructure/CircularBufferCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -8,8 +9,8 @@ namespace Zazz.Infrastructure
     public class CircularBufferCache<TKey, TVal>
     {
         private readonly int _maximumSize;
-        internal ConcurrentDictionary<TKey, TVal> Items;
-        internal ConcurrentDictionary<TKey, int> RequestsCounter;
+        internal readonly ConcurrentDictionary<TKey, TVal> Items;
+        internal readonly ConcurrentDictionary<TKey, int> RequestsCounter;
 
         public CircularBufferCache(int maximumSize)
         {
@@ -20,7 +21,52 @@ namespace Zazz.Infrastructure
 
         public void Add(TKey key, TVal val)
         {
+            if (Items.Count >= _maximumSize)
+                RemoveLowestItem();
+
             Items.TryAdd(key, val);
+            RequestsCounter.TryAdd(key, 0);
+        }
+
+        private void RemoveLowestItem()
+        {
+            if (RequestsCounter.Count < 1)
+                return;
+
+            var key = RequestsCounter
+                .OrderBy(r => r.Value)
+                .First()
+                .Key;
+
+            Remove(key);
+        }
+
+        public void Remove(TKey key)
+        {
+            TVal val;
+            Items.TryRemove(key, out val);
+            
+            int count;
+            RequestsCounter.TryRemove(key, out count);
+        }
+
+        public TVal TryGet(TKey key)
+        {
+            TVal val;
+            Items.TryGetValue(key, out val);
+
+            if (!val.Equals(default(TVal)))
+                IncrementCounter(key);
+
+            return val;
+        }
+
+        private void IncrementCounter(TKey key)
+        {
+            int currentCount;
+            RequestsCounter.TryGetValue(key, out currentCount);
+
+            RequestsCounter.TryUpdate(key, (currentCount + 1), currentCount);
         }
     }
 }
--- tests/Zazz.UnitTests/Infrastructure/CircularBufferCacheShould.cs
+++ tests/Zazz.UnitTests/Infrastructure/CircularBufferCacheShould.cs
@@ -9,7 +9,7 @@ namespace Zazz.UnitTests.Infrastructure
         private CircularBufferCache<string, int> _sut;
         private string _key;
         private int _val;
-        private const int MAXIMUM_SIZE = 50;
+        private const int MAXIMUM_SIZE = 2;
 
         [SetUp]
         public void Init()
@@ -21,7 +21,7 @@ namespace Zazz.UnitTests.Infrastructure
         }
 
         [Test]
-        public void ShouldAddToItemsOnAdd()
+        public void ShouldAddToItemsAndCounterOnAdd()
         {
             //Arrange
             //Act
@@ -30,6 +30,117 @@ namespace Zazz.UnitTests.Infrastructure
             //Assert
             Assert.IsTrue(_sut.Items.ContainsKey(_key));
             Assert.AreEqual(_val, _sut.Items[_key]);
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(_key));
+            Assert.AreEqual(0, _sut.RequestsCounter[_key]);
+        }
+
+        [Test]
+        public void NotThrowIfItemIsAlreadyAdded_OnAdd_ToBeThreadSafe()
+        {
+            //Arrange
+            _sut.Items.TryAdd(_key, _val);
+            _sut.RequestsCounter.TryAdd(_key, 0);
+
+            Assert.IsTrue(_sut.Items.ContainsKey(_key));
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(_key));
+
+            //Act
+            _sut.Add(_key, _val);
+
+            //Assert
+            Assert.Pass("No Exception was thrown");
+        }
+
+        [Test]
+        public void RemoveTheItemWithLowestRequestsCountIfItIsAtMaximumCapacity_OnAdd()
+        {
+            //Arranges
+            var key2 = "key2";
+            var val2 = 2;
+            var key3 = "key3";
+            var val3 = 3;
+
+            _sut.Items.TryAdd(_key, _val);
+            _sut.RequestsCounter.TryAdd(_key, 1);
+            _sut.Items.TryAdd(key2, val2);
+            _sut.RequestsCounter.TryAdd(key2, 2);
+
+            //Act
+            _sut.Add(key3, val3);
+
+            //Assert
+            Assert.IsFalse(_sut.Items.ContainsKey(_key));
+            Assert.IsFalse(_sut.RequestsCounter.ContainsKey(_key));
+            Assert.IsTrue(_sut.Items.ContainsKey(key2));
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(key2));
+            Assert.IsTrue(_sut.Items.ContainsKey(key3));
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(key3));
+        }
+
+        [Test]
+        public void RemoveFromItemsAndCounterOnRemove()
+        {
+            //Arrange
+            _sut.Items.TryAdd(_key, _val);
+            _sut.RequestsCounter.TryAdd(_key, _val);
+
+            Assert.IsTrue(_sut.Items.ContainsKey(_key));
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(_key));
+
+            //Act
+            _sut.Remove(_key);
+
+            //Assert
+            Assert.IsFalse(_sut.Items.ContainsKey(_key));
+            Assert.IsFalse(_sut.RequestsCounter.ContainsKey(_key));
+        }
+
+        [Test]
+        public void ReturnValueIfExists_OnTryGet()
+        {
+            //Arrange
+            _sut.Items.TryAdd(_key, _val);
+            _sut.RequestsCounter.TryAdd(_key, _val);
+            Assert.IsTrue(_sut.Items.ContainsKey(_key));
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(_key));
+
+            //Act
+            var result = _sut.TryGet(_key);
+
+            //Assert
+            Assert.AreEqual(_val, result);
+        }
+
+        [Test]
+        public void ReturnDefaultValueIfNotExists_OnTryGet()
+        {
+            //Arrange
+            Assert.IsFalse(_sut.Items.ContainsKey(_key));
+            Assert.IsFalse(_sut.RequestsCounter.ContainsKey(_key));
+
+            //Act
+            var result = _sut.TryGet(_key);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void IncrementCounterIfExists_OnTryGet()
+        {
+            //Arrange
+            _sut.Items.TryAdd(_key, _val);
+            _sut.RequestsCounter.TryAdd(_key, _val);
+            Assert.IsTrue(_sut.Items.ContainsKey(_key));
+            Assert.IsTrue(_sut.RequestsCounter.ContainsKey(_key));
+
+            var count = _sut.RequestsCounter[_key];
+
+            //Act
+            var result = _sut.TryGet(_key);
+
+            //Assert
+            Assert.AreEqual(++count, _sut.RequestsCounter[_key]);
         }
     }
 }
