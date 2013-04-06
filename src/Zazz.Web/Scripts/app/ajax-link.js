40e54d8828b797fdb846635e4d673716cce2d5cf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-06 13:59:39 +0430

    some bug fixes

--- src/Zazz.Web/Scripts/app/ajax-link.js
+++ src/Zazz.Web/Scripts/app/ajax-link.js
@@ -1,32 +1,43 @@
 ﻿/*
     Parameters:
 
-    data-ajax-request="1" (enabling it)
+    data-ajax="1" (enabling it)
     data-container="#containerId" (id of the container to update)
 
 
 */
 
-$(document).on('cick', '*[data-ajax-request]', function (e) {
+$(document).on('click', 'a[data-ajax]', function (e) {
     e.preventDefault();
 
 
     var self = $(this);
-    var url = self.data('url');
-    var container = $(self.data('container'));
+    
+    var url = self.attr('href');
+    if (!url) {
+        console.log('undefined url');
+        return;
+    }
+    
+    var containerId = self.data('container');
+    if (!containerId) {
+        console.log('undefined container');
+        return;
+    }
 
-    container.css('opacity', '0.6');
+    var container = $(containerId);
+
+    container.css('opacity', '0.5');
 
     $.ajax({
         url: url,
         error: function() {
             toastr.error('An error occured');
+            container.css('opacity', '1');
         },
-        success: function(res) {
-            container.fadeOut(function() {
-                container.html(res);
-                container.fadeIn();
-            });
+        success: function (res) {
+            container.css('opacity', '1');
+            container.html(res);
         }
     });
 })
