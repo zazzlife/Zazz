d37857e760ce79eb2d8369cf11ed3fb90d1fe198
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-09 21:54:16 +0430

    added new repo class

---
+++ src/Zazz.Data/Repositories/FeedPhotoIdRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class FeedPhotoIdRepository : BaseRepository<FeedPhotoId>, IFeedPhotoIdRepository
+    {
+        public FeedPhotoIdRepository(DbContext dbContext) : base(dbContext)
+        {}
+
+        protected override int GetItemId(FeedPhotoId item)
+        {
+            throw new InvalidOperationException("You should always provide the id for updating the record, if it's new then use insert graph.");
+        }
+
+        public void RemoveByPhotoId(int photoId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetCount(int feedId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.IntegrationTests/Repositories/FeedPhotoIdRepositoryShould.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Zazz.Data;
+using Zazz.Data.Repositories;
+
+namespace Zazz.IntegrationTests.Repositories
+{
+    [TestFixture]
+    public class FeedPhotoIdRepositoryShould
+    {
+        private ZazzDbContext _context;
+        private FeedPhotoIdRepository _repo;
+
+        [SetUp]
+        public void Init()
+        {
+            _context = new ZazzDbContext(true);
+            _repo = new FeedPhotoIdRepository(_context);
+        }
+    }
+}
