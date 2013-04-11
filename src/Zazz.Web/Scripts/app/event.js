323470b3d4fe44007706da9ad1eeea0c610630ce
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-11 22:49:26 +0430

    showing event image on hover

--- src/Zazz.Web/Scripts/app/event.js
+++ src/Zazz.Web/Scripts/app/event.js
@@ -1,4 +1,45 @@
-﻿
+﻿var eventLargeImg;
+
+$(document).on('mouseenter', '.event', function () {
+    var self = $(this);
+    var imgHeight = 400;
+
+    var offset = self.offset();
+    var elemWidth = self.width();
+    var elemHeight = self.height();
+    var top, left;
+
+    self.addClass('event-arrow');
+
+    var centerY = offset.top + (elemHeight / 2);
+    top = centerY - (imgHeight / 2);
+
+    left = offset.left + elemWidth + 30;
+
+    eventLargeImg = $('<img />', {
+        src: '/Images/placeholder.gif',
+    });
+
+    eventLargeImg.css({
+        'position': 'absolute',
+        'height': imgHeight + 'px',
+        'top': top,
+        'left': left
+    });
+
+    eventLargeImg.appendTo($('article')).hide().fadeIn('fast');
+});
+
+$(document).on('mouseleave', '.event', function () {
+    $(this).removeClass('event-arrow');
+
+    eventLargeImg.fadeOut('fast', function() {
+        eventLargeImg.remove();
+    });
+});
+
+/////////////////// Flip /////////////////////////
+
 var flipDuration = 250;
 
 function flipToBack(elem, front, back) {
