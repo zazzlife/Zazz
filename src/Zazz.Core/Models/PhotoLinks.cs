ed752b333349a6a6d14b9990f94b088c411a12b5
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-13 13:17:56 +0430

    added photolinks model

---
+++ src/Zazz.Core/Models/PhotoLinks.cs
@@ -0,0 +1,26 @@
+﻿namespace Zazz.Core.Models
+{
+    public class PhotoLinks
+    {
+
+        /// <summary>
+        /// This one is for user image on comments and feeds and like that. It's around 55px
+        /// </summary>
+        public string VerySmallLink { get; set; }
+
+        /// <summary>
+        /// This one is for images on feeds and photo lists and like that. It's around 175px
+        /// </summary>
+        public string SmallLink { get; set; }
+
+        /// <summary>
+        /// This one is for images on feeds and like that. It's around 500px
+        /// </summary>
+        public string MediumLink { get; set; }
+
+        /// <summary>
+        /// This one is for when you show the image in full size, maximum size is around 1600px
+        /// </summary>
+        public string NormalLink { get; set; }
+    }
+}
