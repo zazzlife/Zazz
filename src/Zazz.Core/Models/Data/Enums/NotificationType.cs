4bbb5aa7b55694c0662bd5627809045288f1a9e3
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-23 11:58:19 +0430

    added notification entity

---
+++ src/Zazz.Core/Models/Data/Enums/NotificationType.cs
@@ -0,0 +1,11 @@
+namespace Zazz.Core.Models.Data.Enums
+{
+    public enum NotificationType : byte
+    {
+        FollowRequestAccepted,
+        CommentOnPhoto,
+        CommentOnPost,
+        CommentOnEvent,
+        NewEvent
+    }
+}
---
+++ src/Zazz.Core/Models/Data/Notification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Core.Models.Data
+{
+    public class Notification : BaseEntityLong
+    {
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("AcceptedFollowUserId")]
+        public User AcceptedFollowUser { get; set; }
+
+        [ForeignKey("PhotoId")]
+        public Photo Photo { get; set; }
+
+        [ForeignKey("PostId")]
+        public Post Post { get; set; }
+
+        [ForeignKey("EventId")]
+        public ZazzEvent Event { get; set; }
+
+        public int UserId { get; set; }
+
+        public int? AcceptedFollowUserId { get; set; }
+
+        public int? PhotoId { get; set; }
+
+        public int? PostId { get; set; }
+
+        public int? EventId { get; set; }
+
+        public NotificationType NotificationType { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public bool IsRead { get; set; }
+    }
+}
