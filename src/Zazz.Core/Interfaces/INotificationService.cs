f3785c9c4a56d2508f5c4c84aa33c6eee894ef5a
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-23 15:54:01 +0430

    added notification service

---
+++ src/Zazz.Core/Interfaces/INotificationService.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface INotificationService
+    {
+        IQueryable<Notification> GetUserNotifications(int userId);
+
+        void CreateNotification(Notification notification, bool save = true);
+
+        void RemovePhotoNotifications(int photoId);
+        
+        void RemovePostNotifications(int postId);
+        
+        void RemoveEventNotifications(int eventId);
+
+        void MarkUserNotificationsAsRead(int userId);
+
+        int GetUnreadNotificationsCount(int userId);
+    }
+}
---
+++ src/Zazz.Infrastructure/Services/NotificationService.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IUoW _uow;
+
+        public NotificationService(IUoW uow)
+        {
+            _uow = uow;
+        }
+
+        public IQueryable<Notification> GetUserNotifications(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateNotification(Notification notification, bool save = true)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePhotoNotifications(int photoId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePostNotifications(int postId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveEventNotifications(int eventId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MarkUserNotificationsAsRead(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetUnreadNotificationsCount(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
