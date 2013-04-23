988562e1732c945fe5e90cafedde0a898b5179a4
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-23 12:27:34 +0430

    added notification repo test class

---
+++ tests/Zazz.IntegrationTests/Repositories/NotificationRepositoryShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Zazz.Data;
+using Zazz.Data.Repositories;
+
+namespace Zazz.IntegrationTests.Repositories
+{
+    [TestFixture]
+    public class NotificationRepositoryShould
+    {
+        private ZazzDbContext _context;
+        private NotificationRepository _repo;
+
+        [SetUp]
+        public void Init()
+        {
+            _context = new ZazzDbContext(true);
+            _repo = new NotificationRepository(_context);
+        }
+
+
+    }
+}
