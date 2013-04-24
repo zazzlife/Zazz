cc8fbdd4422839006abaa5a01282fa45db1e6d17
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-24 16:35:32 +0430

    added comment service files.

---
+++ src/Zazz.Infrastructure/Services/CommentService.cs
@@ -0,0 +1,32 @@
+﻿using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly IUoW _uow;
+        private readonly INotificationService _notificationService;
+
+        public CommentService(IUoW uow, INotificationService notificationService)
+        {
+            _uow = uow;
+            _notificationService = notificationService;
+        }
+
+        public void CreateComment(int itemId, CommentType commentType, string comment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditComment(int commentId, int currentUserId, string newComment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveComment(int commentId, int currentUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Infrastructure/Services/CommentServiceShould.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using NUnit.Framework;
+using Zazz.Core.Interfaces;
+using Zazz.Infrastructure.Services;
+
+namespace Zazz.UnitTests.Infrastructure.Services
+{
+    [TestFixture]
+    public class CommentServiceShould
+    {
+        private Mock<IUoW> _uow;
+        private Mock<INotificationService> _notificationService;
+        private CommentService _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _uow = new Mock<IUoW>();
+            _notificationService = new Mock<INotificationService>();
+            _sut = new CommentService(_uow.Object, _notificationService.Object);
+        }
+    }
+}
