65a6ce3d2469a190450e7988d2ab8c252f4c10d1
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-26 13:12:14 +0430

    hiding notifications popover with user clicks away

--- src/Zazz.Web/Scripts/app/global.js
+++ src/Zazz.Web/Scripts/app/global.js
@@ -543,13 +543,32 @@ $(document).on('click', '#load-feeds', function () {
     Notifications
 *********************************/
 
-$(document).on('click', '#notifications-link', function(e) {
-    e.preventDefault();
+var isPopoutVisible = false;
+var clickedAwayFromPopout = false;
 
+$('#notifications-link').popover({
+    html: true,
+    trigger: 'manual'
+}).click(function (e) {
+    $(this).popover('show');
+    clickedAwayFromPopout = false;
+    isPopoutVisible = true;
+    
     $(this).next('.popover').css('width', '555px');
     $(this).next('.popover').css('max-width', '555px');
     $(this).next('.popover').css('min-height', '90px');
 
+    e.preventDefault();
+});
+
+$(document).click(function (e) {
+    if (isPopoutVisible & clickedAwayFromPopout) {
+        $('#notifications-link').popover('hide');
+        isPopoutVisible = clickedAwayFromPopout = false;
+    }
+    else {
+        clickedAwayFromPopout = true;
+    }
 });
 
 $(function () {
