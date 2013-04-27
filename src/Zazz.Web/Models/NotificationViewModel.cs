d0ef0e5e9d025a49a2174496309a272912c1ad82
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-27 16:24:57 +0430

    added notification vm

---
+++ src/Zazz.Web/Models/NotificationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Zazz.Core.Models;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Web.Models
+{
+    public class NotificationViewModel
+    {
+        public int UserId { get; set; }
+
+        public string UserDisplayName { get; set; }
+
+        public PhotoLinks UserPhoto { get; set; }
+
+        public int ItemId { get; set; }
+
+        public NotificationType NotificationType { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
