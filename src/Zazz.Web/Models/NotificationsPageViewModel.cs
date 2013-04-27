b9cc0de9b0ac6f433780a2a769470ac9eaed28b5
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-27 17:55:06 +0430

    small change in the notifications view model

---
+++ src/Zazz.Web/Models/NotificationsPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Zazz.Web.Models
+{
+    public class NotificationsPageViewModel : UserHomeViewModel
+    {
+        public IEnumerable<NotificationViewModel> Notifications { get; set; }
+    }
+}
