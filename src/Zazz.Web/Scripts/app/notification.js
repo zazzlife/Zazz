8f880be5c7164a13330f169a9e9852feaf72e567
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-28 11:33:38 +0430

    moved notification scripts to another file

---
+++ src/Zazz.Web/Scripts/app/notification.js
@@ -0,0 +1,36 @@
+﻿/********************************
+    Popover
+*********************************/
+
+var isPopoutVisible = false;
+var clickedAwayFromPopout = false;
+
+$('#notifications-link').popover({
+    html: true,
+    trigger: 'manual'
+}).click(function (e) {
+    $(this).popover('show');
+    clickedAwayFromPopout = false;
+    isPopoutVisible = true;
+
+    $(this).next('.popover').css('width', '555px');
+    $(this).next('.popover').css('max-width', '555px');
+    $(this).next('.popover').css('min-height', '90px');
+
+    e.preventDefault();
+});
+
+$(document).click(function (e) {
+    if (isPopoutVisible & clickedAwayFromPopout) {
+        $('#notifications-link').popover('hide');
+        isPopoutVisible = clickedAwayFromPopout = false;
+    }
+    else {
+        clickedAwayFromPopout = true;
+    }
+});
+
+/********************************
+    Remove
+*********************************/
+
