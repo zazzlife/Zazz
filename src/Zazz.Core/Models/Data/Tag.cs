5c10aa683049b924572d8b62df11f2ffed231979
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-04-30 13:17:20 +0430

    added tag entity

---
+++ src/Zazz.Core/Models/Data/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zazz.Core.Models.Data
+{
+    public class Tag
+    {
+        public byte Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
