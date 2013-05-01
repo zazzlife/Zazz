f89c0051b12ddf1e05e5ef965d4da3f5f7589663
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-01 11:49:26 +0430

    removed curCSS call from jquery.color

--- src/Zazz.Web/Scripts/jquery.color.js
+++ src/Zazz.Web/Scripts/jquery.color.js
@@ -572,7 +572,7 @@
 					if ( !support.rgba && value._rgba[ 3 ] !== 1 ) {
 						curElem = hook === "backgroundColor" ? elem.parentNode : elem;
 						do {
-							backgroundColor = jQuery.curCSS( curElem, "backgroundColor" );
+							backgroundColor = jQuery.css( curElem, "backgroundColor" );
 						} while (
 							( backgroundColor === "" || backgroundColor === "transparent" ) &&
 							( curElem = curElem.parentNode ) &&
