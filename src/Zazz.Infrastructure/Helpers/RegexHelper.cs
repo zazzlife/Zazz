b7ba9f83c85cf5f5eeb5ec8bd35a4cdae82f25a4
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-01 15:15:13 +0430

    added regex helper class

---
+++ src/Zazz.Infrastructure/Helpers/RegexHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zazz.Infrastructure.Helpers
+{
+    public class RegexHelper
+    {
+        public IEnumerable<string> ExtractTags(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
