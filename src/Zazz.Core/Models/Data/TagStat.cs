0612e5b639587af895a8267bd7aaa1ad96343943
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-04 11:20:25 +0430

    added tag stat entity

---
+++ src/Zazz.Core/Models/Data/TagStat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class TagStat : BaseEntity
+    {
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        
+        public int TagId { get; set; }
+
+        [ForeignKey("TagId")]
+        public virtual Tag Tag { get; set; }
+
+        public int UsersCount { get; set; }
+
+        public virtual ICollection<TagUser> TagUsers { get; set; }
+    }
+}
---
+++ src/Zazz.Core/Models/Data/TagUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class TagUser
+    {
+        [Key, Column(Order = 0)]
+        public int TagStatId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int UserId { get; set; }
+
+        public virtual TagStat TagStat { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
