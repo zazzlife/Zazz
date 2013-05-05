57faf128a129c6e66aaa313ece280493c716cf6a
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-05 20:51:33 +0430

    added post feed delete trigger

---
+++ src/Zazz.Web/App_Data/dbo.TRIGGER_PostFeeds_DELETE.sql
@@ -0,0 +1,12 @@
+﻿---------------------------------------------------
+-- PostFeeds
+--------------------------------------------------
+
+CREATE TRIGGER dbo.TRIGGER_PostFeeds_DELETE
+ON dbo.PostFeeds
+AFTER DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE FROM dbo.Feeds WHERE Id IN(SELECT FeedId FROM deleted);
+END
