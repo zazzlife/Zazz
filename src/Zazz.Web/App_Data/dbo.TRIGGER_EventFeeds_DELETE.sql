6cb31748c76772701be36d0aa0dddd7bc98703ae
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-05 20:50:54 +0430

    added event feeds delete trigger

---
+++ src/Zazz.Web/App_Data/dbo.TRIGGER_EventFeeds_DELETE.sql
@@ -0,0 +1,12 @@
+﻿---------------------------------------------------
+-- EventFeeds
+--------------------------------------------------
+
+CREATE TRIGGER dbo.TRIGGER_EventFeeds_DELETE
+ON dbo.EventFeeds
+AFTER DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE FROM dbo.Feeds WHERE Id IN(SELECT FeedId FROM deleted);
+END
