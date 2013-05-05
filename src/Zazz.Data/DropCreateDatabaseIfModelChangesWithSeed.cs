4e32a4072709c62a2eaedb2a52dd8722170051d8
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-05 18:54:56 +0430

    added an option to execute SQL command on Db Seed Method

--- src/Zazz.Data/DropCreateDatabaseIfModelChangesWithSeed.cs
+++ src/Zazz.Data/DropCreateDatabaseIfModelChangesWithSeed.cs
@@ -1,9 +1,17 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace Zazz.Data
 {
     public class DropCreateDatabaseIfModelChangesWithSeed : DropCreateDatabaseIfModelChanges<ZazzDbContext>
     {
+        private readonly List<string> _sqlCommands;
+
+        public DropCreateDatabaseIfModelChangesWithSeed()
+        {
+            _sqlCommands = new List<string>();
+        }
+
         protected override void Seed(ZazzDbContext context)
         {
             foreach (var city in StaticData.GetCities())
@@ -17,6 +25,9 @@ namespace Zazz.Data
 
             foreach (var tag in StaticData.GetTags())
                 context.Tags.Add(tag);
+
+            foreach (var sqlCommand in _sqlCommands)
+                context.Database.ExecuteSqlCommand(sqlCommand);
 
             base.Seed(context);
         }
