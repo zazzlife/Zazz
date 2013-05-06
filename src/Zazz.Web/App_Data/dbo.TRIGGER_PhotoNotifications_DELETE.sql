c9bbd3750fa8a9593d9cf71b38c3d3ac3be65eaa
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-06 13:35:16 +0430

    added photo notifications delete trigger

---
+++ src/Zazz.Web/App_Data/dbo.TRIGGER_PhotoNotifications_DELETE.sql
@@ -0,0 +1,12 @@
+﻿---------------------------------------------------
+-- PhotoNotifications
+--------------------------------------------------
+
+CREATE TRIGGER dbo.TRIGGER_PhotoNotifications_DELETE
+ON dbo.PhotoNotifications
+AFTER DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE FROM dbo.Notifications WHERE Id IN(SELECT NotificationId FROM deleted);
+END
