7e7b6ee846e625a78b62196b3942b3d1137dfbde
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-06 13:34:30 +0430

    added PostNotifications delete trigger

---
+++ src/Zazz.Web/App_Data/dbo.TRIGGER_PostNotifications_DELETE.sql
@@ -0,0 +1,12 @@
+﻿---------------------------------------------------
+-- PostNotifications
+--------------------------------------------------
+
+CREATE TRIGGER dbo.TRIGGER_PostNotifications_DELETE
+ON dbo.PostNotifications
+AFTER DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE FROM dbo.Notifications WHERE Id IN(SELECT NotificationId FROM deleted);
+END
