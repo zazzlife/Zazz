1681d6edb96ed116662e2e8f978d1e560ff83120
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-07 17:19:00 +0430

    implemented tag statistics service methods.

--- src/Zazz.BackgroundServices/TagStatisticsService.cs
+++ src/Zazz.BackgroundServices/TagStatisticsService.cs
@@ -1,26 +1,59 @@
-﻿using System.ServiceProcess;
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+using Zazz.Core.Interfaces;
+using Zazz.Data;
+using Zazz.Infrastructure.Services;
 
 namespace Zazz.BackgroundServices
 {
     public class TagStatisticsService : ServiceBase
     {
+        private readonly Timer _timer = new Timer(1000 * 60 * 5);
+        private readonly IUoW _uow;
+        private readonly IStaticDataRepository _staticDataRepo;
+        private readonly TagService _tagService;
+
+        public TagStatisticsService()
+        {
+            //TODO: Move this initializations to an IoC container.
+            _uow = new UoW();
+            _staticDataRepo = new StaticDataRepository();
+            _tagService = new TagService(_uow, _staticDataRepo);
+
+            _timer.Elapsed += OnTimerElapsed;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            _tagService.UpdateTagStatistics();
+        }
+
         protected override void OnPause()
         {
+            _timer.Stop();
+
             base.OnPause();
         }
 
         protected override void OnContinue()
         {
+            _timer.Start();
+
             base.OnContinue();
         }
 
         protected override void OnStart(string[] args)
         {
+            _timer.Start();
+
             base.OnStart(args);
         }
 
         protected override void OnStop()
         {
+            _timer.Stop();
+
             base.OnStop();
         }
     }
