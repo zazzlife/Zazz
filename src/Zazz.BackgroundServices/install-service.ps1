41cce09cc99e10dc0cbd62c7a62d6f5e2548ff86
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-07 21:05:13 +0430

    added powershell script to install the service.

---
+++ src/Zazz.BackgroundServices/install-service.ps1
@@ -0,0 +1,12 @@
+﻿param([string] $p)
+
+$fileExists = (Test-Path -Path $p);
+
+if ($fileExists) 
+{
+    New-Service -Name "Zazz.BackgroundServices" -BinaryPathName $p -DisplayName "Zazz Background Services" -StartupType Automatic -Verbose
+}
+else 
+{
+    Write-Host "File not found!" -ForegroundColor Red;
+}
