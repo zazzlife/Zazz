c8a8a99eee84d0a816287ced91a359f7c5ad2c2b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-07 17:07:54 +0430

    added background services references

--- src/Zazz.BackgroundServices/TagStatisticsService.cs
+++ src/Zazz.BackgroundServices/TagStatisticsService.cs
@@ -4,6 +4,24 @@ namespace Zazz.BackgroundServices
 {
     public class TagStatisticsService : ServiceBase
     {
-         
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
     }
 }
