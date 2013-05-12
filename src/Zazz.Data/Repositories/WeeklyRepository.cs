d529036660f06a6c3c4fb4cf56649a53554d2a3f
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-12 13:14:08 +0430

    added weekly repository

---
+++ src/Zazz.Data/Repositories/WeeklyRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class WeeklyRepository : BaseRepository<Weekly>, IWeeklyRepository
+    {
+        public WeeklyRepository(DbContext dbContext) : base(dbContext)
+        {}
+
+        protected override int GetItemId(Weekly item)
+        {
+            throw new InvalidOperationException("You must provide the Id for updating. Use InsertGraph for insert");
+        }
+    }
+}
