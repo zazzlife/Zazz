360f18b41cbcef7a4f72728d668aab08263a726c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-12 12:50:11 +0430

    added weekly entity

---
+++ src/Zazz.Core/Models/Data/Weekly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class Weekly
+    {
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public DayOfWeek DayOfTheWeek { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [ForeignKey("Photo")]
+        public int PhotoId { get; set; }
+
+        [MaxLength(4000)]
+        public string Description { get; set; }
+
+        public User User { get; set; }
+
+        public Photo Photo { get; set; }
+    }
+}
