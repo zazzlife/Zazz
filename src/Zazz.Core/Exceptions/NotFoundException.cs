e6ca437a79031f39d9373287bb66054a4f0d7520
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-13 21:40:05 +0430

    added not found exception

---
+++ src/Zazz.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Zazz.Core.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public NotFoundException()
+        {}
+
+        public NotFoundException(string message) : base(message)
+        {}
+
+        public NotFoundException(string message, Exception inner) : base(message, inner)
+        {}
+
+        protected NotFoundException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {}
+    }
+}
