5acd75763e9a6a2648824a32b4c150f79639ca64
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-18 09:27:27 +0430

    added app and access token entities

---
+++ src/Zazz.Core/Models/Data/AccessToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zazz.Core.Models.Data
+{
+    public class AccessToken : BaseEntity
+    {
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [ForeignKey("App")]
+        public int AppId { get; set; }
+
+        [MaxLength(4000)]
+        public string Token { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual App App { get; set; }
+
+        public DateTime IssuedTime { get; set; }
+    }
+}
