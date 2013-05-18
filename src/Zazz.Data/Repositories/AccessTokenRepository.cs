57ee622c90117524c46832bef904916220a33504
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-18 09:47:36 +0430

    added repositories for the new entities

---
+++ src/Zazz.Data/Repositories/AccessTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class AccessTokenRepository : BaseRepository<AccessToken>, IAccessTokenRepository
+    {
+        public AccessTokenRepository(DbContext dbContext) : base(dbContext)
+        {}
+
+        protected override int GetItemId(AccessToken item)
+        {
+            throw new InvalidOperationException("You must provide the Id for updating. If you need to insert use InsertGraph");
+        }
+    }
+}
---
+++ src/Zazz.Data/Repositories/AppRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Data.Repositories
+{
+    public class AppRepository : BaseRepository<App>, IAppRepository
+    {
+        public AppRepository(DbContext dbContext)
+            : base(dbContext)
+        { }
+    
+        protected override int GetItemId(App item)
+        {
+            throw new InvalidOperationException("You must provide the Id for updating. If you need to insert use InsertGraph");
+        }
+    }
+}
