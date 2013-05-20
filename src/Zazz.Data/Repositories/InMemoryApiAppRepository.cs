91ffa7bbe94f293db9e9456dede817cd0360f5f5
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-20 11:07:26 +0430

    added HMAC authorize attribute

--- src/Zazz.Data/Repositories/InMemoryApiAppRepository.cs
+++ src/Zazz.Data/Repositories/InMemoryApiAppRepository.cs
@@ -12,24 +12,24 @@ namespace Zazz.Data.Repositories
 
         public InMemoryApiAppRepository()
         {
-            var iosKey = "7jFgrLKqfIO9YEkTqEZZQ+yVnHLZK2jm2kQEtQrpcjGIaRmQ0xCrdB9PCNJ/nbwUThaNC5ZSQi2cVcs2e232eDrz67xcmWL2qpVwuTN3zpBOIaka0FNkCinjN6SF5sZQUB/gga9KFJYzFNCe8k4nHteblxVZCujmB5ohKQYwB5k=";
+            const string IOS_KEY = "7jFgrLKqfIO9YEkTqEZZQ+yVnHLZK2jm2kQEtQrpcjGIaRmQ0xCrdB9PCNJ/nbwUThaNC5ZSQi2cVcs2e232eDrz67xcmWL2qpVwuTN3zpBOIaka0FNkCinjN6SF5sZQUB/gga9KFJYzFNCe8k4nHteblxVZCujmB5ohKQYwB5k=";
 
-            var androidKey = "IwLhcyZh31T1pfyFRPCRo3SIWhgufosBUzED7Kan25CM6arrZPHWHszgaF+9Z43OrEfNCQ7INBsp6YXCE0dvBEWLvZOIJJOifEo4cOHwEpBJ9PnqVV32ebXySbKoCGKwafLGMdD1uFgmmQQ6altMwvFeid2RX1u0rM/rNwEUMhI=";
+            const string ANDROID_KEY = "IwLhcyZh31T1pfyFRPCRo3SIWhgufosBUzED7Kan25CM6arrZPHWHszgaF+9Z43OrEfNCQ7INBsp6YXCE0dvBEWLvZOIJJOifEo4cOHwEpBJ9PnqVV32ebXySbKoCGKwafLGMdD1uFgmmQQ6altMwvFeid2RX1u0rM/rNwEUMhI=";
 
             var iosApp = new ApiApp
                          {
                              Id = 1,
                              Name = "Zazz for iOS",
-                             PasswordSigningKey = Convert.FromBase64String(iosKey),
-                             RequestSigningKey = Convert.FromBase64String(iosKey)
+                             PasswordSigningKey = Convert.FromBase64String(IOS_KEY),
+                             RequestSigningKey = Convert.FromBase64String(IOS_KEY)
                          };
 
             var androidApp = new ApiApp
                              {
                                  Id = 2,
                                  Name = "Zazz for Android",
-                                 PasswordSigningKey = Convert.FromBase64String(androidKey),
-                                 RequestSigningKey = Convert.FromBase64String(androidKey)
+                                 PasswordSigningKey = Convert.FromBase64String(ANDROID_KEY),
+                                 RequestSigningKey = Convert.FromBase64String(ANDROID_KEY)
                              };
 
             ApiApps.Add(iosApp);
---
+++ src/Zazz.Web/Filters/HMACAuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Zazz.Web.Filters
+{
+    public class HMACAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            return base.IsAuthorized(actionContext);
+        }
+    }
+}
