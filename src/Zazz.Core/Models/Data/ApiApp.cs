c3139a581996a1c1579a2f1151dff4ca419b211a
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-20 09:41:41 +0430

    added api app entity

---
+++ src/Zazz.Core/Models/Data/ApiApp.cs
@@ -0,0 +1,11 @@
+﻿namespace Zazz.Core.Models.Data
+{
+    public class ApiApp : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public byte[] PasswordSigningKey { get; set; }
+
+        public byte[] RequestSigningKey { get; set; } 
+    }
+}
