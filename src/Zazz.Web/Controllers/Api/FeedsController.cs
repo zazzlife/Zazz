f3d42023db631fc2b6bc67f2a2929a7c91556ff7
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-22 16:04:38 +0430

    added base feed api response + default get methods for api controller

--- src/Zazz.Web/Controllers/Api/FeedsController.cs
+++ src/Zazz.Web/Controllers/Api/FeedsController.cs
@@ -5,10 +5,44 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Filters;
+using Zazz.Core.Interfaces;
+using Zazz.Web.Filters;
+using Zazz.Web.Models.Api;
 
 namespace Zazz.Web.Controllers.Api
 {
+    [HMACAuthorize]
     public class FeedsController : BaseApiController
     {
+        private readonly IUoW _uow;
+        private readonly IUserService _userService;
+        private readonly IPhotoService _photoService;
+
+        public FeedsController(IUoW uow, IUserService userService, IPhotoService photoService)
+        {
+            _uow = uow;
+            _userService = userService;
+            _photoService = photoService;
+        }
+
+        public IEnumerable<object> GetHomeFeeds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetHomeFeeds(int lastFeed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetUserFeeds(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetUserFeeds(int id, int lastFeed)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/Zazz.Web/Models/Api/Feed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Zazz.Core.Models;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Web.Models.Api
+{
+    public abstract class BaseFeedApiResponse
+    {
+        public int UserId { get; set; }
+
+        public PhotoLinks UserDisplayPhoto { get; set; }
+
+        public string UserDisplayName { get; set; }
+
+        public bool CanCurrentUserRemoveFeed { get; set; }
+
+        public FeedType FeedType { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
