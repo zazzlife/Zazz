675420c3f23abcc3f4cc0e875379820a726a1ea2
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-22 15:18:57 +0430

    added a base api controller

---
+++ src/Zazz.Web/Controllers/Api/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public abstract class BaseApiController : ApiController
+    {
+         int ExtractUserIdFromHeader()
+         {
+             var auth = Request.Headers.Authorization;
+             if (auth == null || String.IsNullOrEmpty(auth.Parameter))
+                 return 0;
+
+             var authSegments = auth.Parameter.Split(':');
+             if (authSegments.Length < 4)
+                 return 0;
+
+             int userId;
+             Int32.TryParse(authSegments[2], out userId);
+
+             return userId;
+         }
+    }
+}
---
+++ src/Zazz.Web/Controllers/Api/FeedsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class FeedsController : BaseApiController
+    {
+    }
+}
