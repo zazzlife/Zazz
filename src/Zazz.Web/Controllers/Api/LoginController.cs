82f375b0993499a45c1ec3f05d8987ca229bde8c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-22 10:21:13 +0430

    added login api controller

---
+++ src/Zazz.Web/Controllers/Api/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class LoginController : ApiController
+    {
+    }
+}
