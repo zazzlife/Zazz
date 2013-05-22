106bd95a9cba12c2e78890c69ce95e87af8da859
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-22 15:52:13 +0430

    serializing enums as string instead of their number

--- src/Zazz.Web/App_Start/JsonConfig.cs
+++ src/Zazz.Web/App_Start/JsonConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 
 namespace Zazz.Web
@@ -9,6 +10,7 @@ namespace Zazz.Web
         {
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
         }
     }
 }
