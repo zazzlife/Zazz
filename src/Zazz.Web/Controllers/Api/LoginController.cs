101825ceaa8801059f799eb3b9d693c525d8ecb9
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-22 10:27:54 +0430

    added login models and action

--- src/Zazz.Web/Controllers/Api/LoginController.cs
+++ src/Zazz.Web/Controllers/Api/LoginController.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Zazz.Web.Models.Api;
 
 namespace Zazz.Web.Controllers.Api
 {
     public class LoginController : ApiController
     {
+        public LoginApiResponse Get(LoginApiRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/Zazz.Web/Models/Api/Login.cs
@@ -0,0 +1,25 @@
+﻿using Zazz.Core.Models;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Web.Models.Api
+{
+    public class LoginApiRequest
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class LoginApiResponse
+    {
+        public int Id { get; set; }
+
+        public AccountType AccountType { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public PhotoLinks DisplayPicture { get; set; }
+    }
+}
