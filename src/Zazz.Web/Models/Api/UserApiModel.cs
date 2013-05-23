310af17228984a79b1599863cbaadd38f544a4f7
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-23 15:48:35 +0430

    added api user models

---
+++ src/Zazz.Web/Models/Api/UserApiModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Zazz.Core.Models;
+using Zazz.Core.Models.Data;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Web.Models.Api
+{
+    public class ApiUser
+    {
+        public int Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public PhotoLinks DisplayPhoto { get; set; }
+
+        public AccountType AccountType { get; set; }
+
+        public int FollowersCount { get; set; }
+
+        public bool IsSelf { get; set; }
+
+        public bool IsTargetUserFollowingCurrentUser { get; set; }
+
+        public bool IsCurrentUserFollowingTargetUser { get; set; }
+
+        public bool FollowRequestAlreadySent { get; set; }
+
+        public IEnumerable<FeedApiResponse> Feeds { get; set; }
+
+        public IEnumerable<PhotoApiModel> Photos { get; set; }
+    }
+
+    public class ApiUserDetails
+    {
+        public City City { get; set; }
+
+        public Major Major { get; set; }
+
+        public School School { get; set; }
+
+        public string FullName { get; set; }
+
+        public Gender Gender { get; set; }
+    }
+
+    public class ApiClubDetails
+    {
+        public ClubType ClubType { get; set; }
+
+        public string Address { get; set; }
+
+        public string ClubName { get; set; }
+
+        public PhotoLinks CoverPhoto { get; set; }
+    }
+}
