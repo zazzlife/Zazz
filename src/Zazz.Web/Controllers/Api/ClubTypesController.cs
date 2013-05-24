a1512377d1e2da1c2ce433c4e254b32de9058abe
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-24 21:07:48 +0430

    added club types api controller

---
+++ src/Zazz.Web/Controllers/Api/ClubTypesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class ClubTypesController : ApiController
+    {
+        private readonly IStaticDataRepository _staticDataRepository;
+
+        public ClubTypesController(IStaticDataRepository staticDataRepository)
+        {
+            _staticDataRepository = staticDataRepository;
+        }
+
+        // GET api/v1/clubtypes
+        public IEnumerable<ClubType> Get()
+        {
+            return _staticDataRepository.GetClubTypes();
+        }
+
+        // GET api/clubtypes/5
+        public ClubType Get(int id)
+        {
+            return _staticDataRepository.GetClubTypes()
+                                        .SingleOrDefault(t => t.Id == id);
+        }
+    }
+}
