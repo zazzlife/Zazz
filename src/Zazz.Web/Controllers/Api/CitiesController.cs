b1ec0d92b7a2f476fb5a76c5272fd937d0847a27
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-24 20:42:42 +0430

    added cities api controller

---
+++ src/Zazz.Web/Controllers/Api/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class CitiesController : ApiController
+    {
+        private readonly IStaticDataRepository _staticDataRepository;
+
+        public CitiesController(IStaticDataRepository staticDataRepository)
+        {
+            _staticDataRepository = staticDataRepository;
+        }
+
+        // GET api/v1/cities
+        public IEnumerable<City> Get()
+        {
+            return _staticDataRepository.GetCities();
+        }
+
+        // GET api/v1/cities/5
+        public City Get(int id)
+        {
+            return _staticDataRepository.GetCities()
+                                        .SingleOrDefault(c => c.Id == id);
+        }
+    }
+}
