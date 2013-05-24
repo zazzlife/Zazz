7793b4492ea5a27d7194c5630adfd87fe812eb39
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-24 20:43:59 +0430

    added schools api controller

---
+++ src/Zazz.Web/Controllers/Api/SchoolsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class SchoolsController : ApiController
+    {
+        private readonly IStaticDataRepository _staticDataRepository;
+
+        public SchoolsController(IStaticDataRepository staticDataRepository)
+        {
+            _staticDataRepository = staticDataRepository;
+        }
+
+        // GET api/v1/cities
+        public IEnumerable<School> Get()
+        {
+            return _staticDataRepository.GetSchools();
+        }
+
+        // GET api/v1/cities/5
+        public School Get(int id)
+        {
+            return _staticDataRepository.GetSchools()
+                                        .SingleOrDefault(s => s.Id == id);
+        }
+    }
+}
