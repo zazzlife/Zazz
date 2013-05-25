f7998ddbfdd589f41352cc288fe20b27bcb12869
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-25 13:47:46 +0430

    added album api controller + api album model

---
+++ src/Zazz.Web/Controllers/Api/AlbumController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class AlbumController : BaseApiController
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+        // GET api/v1/album/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/album
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/album/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/album/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Zazz.Web/Models/Api/ApiAlbum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Zazz.Core.Models;
+
+namespace Zazz.Web.Models.Api
+{
+    public class ApiAlbum
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public PhotoLinks Thumbnail { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public IEnumerable<PhotoApiModel> Photos { get; set; }
+    }
+}
