ae31bc4fb30cfac932317c253e48c8136d4d6f5d
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-26 17:01:36 +0430

    added a base controller for pages with user page layout

---
+++ src/Zazz.Web/Controllers/UserPageLayoutBaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Web.Controllers
+{
+    public abstract class UserPageLayoutBaseController : BaseController
+    {
+        protected readonly ITagService TagService;
+
+        protected UserPageLayoutBaseController(IUserService userService, IPhotoService photoService,
+                                            IDefaultImageHelper defaultImageHelper, ITagService tagService)
+            : base(userService, photoService, defaultImageHelper)
+        {
+            TagService = tagService;
+        }
+
+        public IEnumerable<TagStat> GetTagStats()
+        {
+            return TagService.GetAllTagStats();
+        }
+    }
+}
