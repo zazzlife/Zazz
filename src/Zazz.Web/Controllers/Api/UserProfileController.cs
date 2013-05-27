a5a415b09e7254fed07454a0af0df96e4615722f
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-27 14:34:39 +0430

    added api user profile controller

---
+++ src/Zazz.Web/Controllers/Api/UserProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class UserProfileController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public UserProfileController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET api/v1/userprofile
+        public ApiUserProfile Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/v1/userprofile/5
+        public ApiUserProfile Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
