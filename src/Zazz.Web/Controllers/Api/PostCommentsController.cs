aa48ca40e2a0092b7470d4546c3d32f451939dba
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-28 19:18:09 +0430

    added post comments controller + unit tests

--- src/Zazz.Web/Controllers/Api/PostCommentsController.cs
+++ src/Zazz.Web/Controllers/Api/PostCommentsController.cs
@@ -1,9 +1,75 @@
-﻿using Zazz.Core.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+using Zazz.Core.Models.Data.Enums;
 using Zazz.Web.Filters;
+using Zazz.Web.Interfaces;
+using Zazz.Web.Models.Api;
 
 namespace Zazz.Web.Controllers.Api
 {
     [HMACAuthorize]
     public class PostCommentsController : BaseApiController
-    {}
+    {
+        private readonly IFeedHelper _feedHelper;
+        private readonly ICommentService _commentService;
+
+        public PostCommentsController(IFeedHelper feedHelper, ICommentService commentService)
+        {
+            _feedHelper = feedHelper;
+            _commentService = commentService;
+        }
+
+        // GET /api/v1/postcomments/5
+
+        public IEnumerable<ApiComment> Get(int id, int lastComment = 0)
+        {
+            if (id == 0 || lastComment < 0)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            return _feedHelper.GetComments(id, CommentType.Post, ExtractUserIdFromHeader(), lastComment, 10)
+                              .Select(_feedHelper.CommentViewModelToApiModel);
+        }
+
+        // POST /api/v1/postcomments/5
+
+        public ApiComment Post(int id, [FromBody] string comment)
+        {
+            if (id == 0 || String.IsNullOrWhiteSpace(comment))
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var c = new Comment
+                    {
+                        Message = comment,
+                        PostComment = new PostComment()
+                                      {
+                                          PostId = id
+                                      },
+                        Time = DateTime.UtcNow,
+                        UserId = ExtractUserIdFromHeader()
+                    };
+
+            try
+            {
+                var cid = _commentService.CreateComment(c, CommentType.Post);
+
+                return new ApiComment
+                       {
+                           CommentId = cid,
+                           CommentText = comment,
+                           IsFromCurrentUser = true,
+                           Time = c.Time,
+                           UserId = ExtractUserIdFromHeader()
+                       };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
 }
