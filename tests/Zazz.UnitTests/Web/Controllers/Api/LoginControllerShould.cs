73ad995845e311848f4b54807bfcda8f093e8b17
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-28 11:53:37 +0430

    post controller test setup

--- tests/Zazz.UnitTests/Web/Controllers/Api/LoginControllerShould.cs
+++ tests/Zazz.UnitTests/Web/Controllers/Api/LoginControllerShould.cs
@@ -42,12 +42,7 @@ namespace Zazz.UnitTests.Web.Controllers.Api
         [SetUp]
         public void Init()
         {
-            _testApp = new ApiApp
-                       {
-                           Id = 1,
-                           PasswordSigningKey = Convert.FromBase64String("g0uq3k4Qe0cbKO+CSC+hOt+Gkba+cuIWTBk9G9RSmWpv5UTcsAxpWAHub2+PhXYWZsPGnvLQuyAF3hAbae827v8OIza+j1+Gq4mxOZpPZBwOhPZmIkpntUE5VyXBIYzsqFC966VywlQB8XxQMUUe4A8ziH3ux0qsudYL26IuODo="),
-                           RequestSigningKey = Convert.FromBase64String("tsz7eTBzJEv9s3oOu9WsICuMbGZcaj8d80LFHjm9ZqmhCM71wIdd5iHcA6Mq/MO+n71qXKidQqrhKDg/aCwOzJrh0AnTLZkCcV/uKt7XmT8kF7dlpop2F/2QVqt9WQQV2hGHbgH4kdkQfQGGFXHI37lqlsWRyiQxaGuJa1hINj4=")
-                       };
+            _testApp = Mother.GetApiApp();
 
             _mockRepo = new MockRepository(MockBehavior.Strict);
             _userService = _mockRepo.Create<IUserService>();
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/PostControllerShould.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.Filters;
+using System.Web.Http.SelfHost;
+using Moq;
+using NUnit.Framework;
+using StructureMap;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+using Zazz.Infrastructure.Services;
+using Zazz.Web;
+using Zazz.Web.DependencyResolution;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class PostControllerShould
+    {
+        private HttpSelfHostServer _handler;
+        private HttpClient _client;
+        private MockRepository _mockRepo;
+        private Mock<IUserService> _userService;
+        private Mock<IApiAppRepository> _appRepo;
+        private Mock<IPhotoService> _photoService;
+        private ApiApp _app;
+        private CryptoService _cryptoService;
+        private string _password;
+        private User _user;
+        private Mock<IPostService> _postService;
+
+        [SetUp]
+        public void Init()
+        {
+            //Global api config
+
+            _app = Mother.GetApiApp();
+            _mockRepo = new MockRepository(MockBehavior.Strict);
+            _userService = _mockRepo.Create<IUserService>();
+            _appRepo = _mockRepo.Create<IApiAppRepository>();
+            _photoService = _mockRepo.Create<IPhotoService>();
+            _cryptoService = new CryptoService();
+
+            _password = "password";
+            string iv;
+            var passwordCipher = _cryptoService.EncryptPassword(_password, out iv);
+
+            _user = new User
+                    {
+                        Id = 12,
+                        Password = passwordCipher,
+                        PasswordIV = Convert.FromBase64String(iv)
+                    };
+
+
+            const string BASE_ADDRESS = "http://localhost:8080";
+            var config = new HttpSelfHostConfiguration(BASE_ADDRESS);
+            JsonConfig.Configure(config);
+            WebApiConfig.Register(config);
+            var iocContainer = BuildIoC();
+            config.DependencyResolver = new StructureMapDependencyResolver(iocContainer);
+
+            _handler = new HttpSelfHostServer(config);
+            _client = new HttpClient(_handler);
+
+            //end of global config
+
+            _postService = _mockRepo.Create<IPostService>();
+        }
+
+        private IContainer BuildIoC()
+        {
+            return new Container(x =>
+                                 {
+                                     x.For<IFilterProvider>().Use<StructureMapFilterProvider>();
+                                     x.For<ICryptoService>().Use<CryptoService>();
+                                     x.For<IUserService>().Use(_userService.Object);
+                                     x.For<IApiAppRepository>().Use(_appRepo.Object);
+                                     x.For<IPhotoService>().Use(_photoService.Object);
+                                     x.For<IPostService>().Use(_postService.Object);
+                                 });
+        }
+    }
+}
