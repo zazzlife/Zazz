41b4699114a547d8281495d75bcc9ce1fa093ba9
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-29 13:53:10 +0430

    added api notifications model + controller

---
+++ src/Zazz.Web/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class NotificationsController : ApiController
+    {
+        // GET api/v1/notifications
+        public IEnumerable<ApiNotification> Get(int? lastNotification)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/v1/notifications/5
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Zazz.Web/Models/Api/ApiNotification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Zazz.Core.Models;
+using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Web.Models.Api
+{
+    public class ApiNotification
+    {
+        public long Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string UserDisplayName { get; set; }
+
+        public PhotoLinks UserPhoto { get; set; }
+
+        public NotificationType NotificationType { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public ApiPhoto Photo { get; set; }
+
+        public ApiPost Post { get; set; }
+
+        public ApiEvent Event { get; set; }
+    }
+}
