a7ee41be311b8128d09b199b7eacb945a1877783
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-05-30 12:57:11 +0430

    added api events controller

---
+++ src/Zazz.Web/Controllers/Api/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Web.Filters;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [HMACAuthorize]
+    public class EventsController : BaseApiController
+    {
+        // GET api/v1/events/5
+        public ApiEvent Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/v1/events
+        public ApiEvent Post([FromBody]string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/v1/events/5
+        public void Put(int id, [FromBody]string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/v1/events/5
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/EventsControllerShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    public class EventsControllerShould : BaseHMACTests
+    {
+        private int _eventId;
+
+        public override void Init()
+        {
+            base.Init();
+
+            _eventId = 332;
+            ControllerAddress = "/api/v1/events/" + _eventId;
+
+            IocContainer.Configure(x =>
+            {
+            });
+        }
+    }
+}
