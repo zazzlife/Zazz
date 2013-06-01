0567a505acff212e33d0add3f3aa60cd30b625a5
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-01 17:23:06 +0430

    added azure blob storage service

---
+++ src/Zazz.Core/Interfaces/IStorageService.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IStorageService
+    {
+        void SavePhotoBlob(string fileName, Stream data);
+
+        void SaveBlob(string containerName, string fileName, Stream data);
+    }
+}
---
+++ src/Zazz.Infrastructure/Services/AzureService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class AzureService : IStorageService
+    {
+        private readonly string _storageConnString;
+
+        private const string PIC_CONTAINER_NAME = "pictures";
+
+        public AzureService(string storageConnString)
+        {
+            _storageConnString = storageConnString;
+        }
+
+        public void SavePhotoBlob(string fileName, Stream data)
+        {
+            SaveBlob(PIC_CONTAINER_NAME, fileName, data);
+        }
+
+        public void SaveBlob(string containerName, string fileName, Stream data)
+        {
+            var storageAccount = CloudStorageAccount.Parse(_storageConnString);
+            var client = storageAccount.CreateCloudBlobClient();
+
+            var container = client.GetContainerReference(containerName);
+            var blob = container.GetBlockBlobReference(fileName);
+
+            blob.UploadFromStream(data);
+        }
+    }
+}
