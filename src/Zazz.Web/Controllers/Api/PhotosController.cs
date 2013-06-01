902487be6ab853f0c0dcd284700e3a83099f4521
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-01 18:19:18 +0430

    added photos api controller

---
+++ src/Zazz.Web/Controllers/Api/PhotosController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Web.Filters;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [HMACAuthorize]
+    public class PhotosController : BaseApiController
+    {
+        private readonly IPhotoService _photoService;
+
+        public PhotosController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
+
+        // GET api/v1/photos?userId=&lastPhoto=
+        public IEnumerable<ApiPhoto> Get(int userId, int? lastPhoto)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/v1/photos/5
+        public ApiPhoto Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/v1/photos
+        public void Post([FromBody]ApiPhoto p)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/v1photos/5
+        public void Put(int id, [FromBody]ApiPhoto p)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/v1/photos/5
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/PhotosControllerShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class PhotosControllerShould : BaseHMACTests
+    {
+        private int _photoId;
+        private Mock<IPhotoService> _photoService;
+        private ApiPhoto _apiPhoto;
+
+        public override void Init()
+        {
+            base.Init();
+
+            _photoId = 99;
+            ControllerAddress = "/api/v1/photos/" + _photoId;
+
+            _apiPhoto = new ApiPhoto
+                        {
+                            Description = "desc",
+                        };
+
+            _photoService = MockRepo.Create<IPhotoService>();
+
+            IocContainer.Configure(x =>
+            {
+                x.For<IPhotoService>().Use(_photoService.Object);
+            });
+        }
+    }
+}
