c8217dfff77dfb07ee9ce1f5212ebcbd1257cfef
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-02 14:44:57 +0430

    added new image validator method.

--- src/Zazz.Web/ImageValidator.cs
+++ src/Zazz.Web/ImageValidator.cs
@@ -1,14 +1,34 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Web;
 
 namespace Zazz.Web
 {
-    public class ImageValidator
+    public static class ImageValidator
     {
+        private const int MAXIMUM_SIZE = 3 * 1024 * 1024;
+
+        public static bool IsValid(byte[] img)
+        {
+            if (img.Length > MAXIMUM_SIZE)
+                return false;
+
+            try
+            {
+                using (var i = Image.FromStream(new MemoryStream(img)))
+                    return i.RawFormat.Equals(ImageFormat.Jpeg);
+            }
+            catch 
+            {}
+
+            return false;
+        }
+
         public static bool IsValid(HttpPostedFileBase uploadedImage, out string errorMessage)
         {
-            if (uploadedImage.ContentLength > 3 * 1024 * 1024)
+            if (uploadedImage.ContentLength > MAXIMUM_SIZE)
             {
                 errorMessage = "Image should not be larger than 3mb";
                 return false;
