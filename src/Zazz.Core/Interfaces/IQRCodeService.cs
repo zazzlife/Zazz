95f5eea08ef70e94d9f3744a1fd105fdea085da9
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-04 11:06:32 +0430

    added qr code service.

---
+++ src/Zazz.Core/Interfaces/IQRCodeService.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IQRCodeService
+    {
+        MemoryStream Generate(string text);
+    }
+}
---
+++ src/Zazz.Infrastructure/Services/QRCodeService.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.Infrastructure.Services
+{
+    public class QRCodeService : IQRCodeService
+    {
+        public MemoryStream Generate(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
