11306caa735b6e3e089c742cfa71bf012c89fb5c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-04 15:56:05 +0430

    added qr code controller

---
+++ src/Zazz.Web/Controllers/Api/QRCodeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Web.Filters;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [HMACAuthorize]
+    public class QRCodeController : ApiController
+    {
+        // GET api/v1/qrcode
+        public HttpResponseMessage Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/QRCodeControllerShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class QRCodeControllerShould : BaseHMACTests
+    {
+        public override void Init()
+        {
+            base.Init();
+
+            ControllerAddress = "/api/v1/qrcode";
+
+            IocContainer.Configure(x =>
+            {
+            });
+        }
+    }
+}
