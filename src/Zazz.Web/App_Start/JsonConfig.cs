e9b0bbb31900847fe2e8edf1e64157a9ec5fe8e6
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-04 17:07:14 +0430

    changed some of default json.net configs

--- src/Zazz.Web/App_Start/JsonConfig.cs
+++ src/Zazz.Web/App_Start/JsonConfig.cs
@@ -13,6 +13,17 @@ namespace Zazz.Web
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
             config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
+
+            var defaultJsonSettings = new JsonSerializerSettings
+                                                {
+                                                    NullValueHandling = NullValueHandling.Ignore,
+                                                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                                                };
+
+            defaultJsonSettings.Converters.Add(new StringEnumConverter());
+
+            JsonConvert.DefaultSettings = () => defaultJsonSettings;
         }
     }
 }
