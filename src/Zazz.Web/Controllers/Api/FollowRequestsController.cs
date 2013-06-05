f26b3015db6ed68b8eacfc15e1b533e00530422b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-05 13:06:00 +0430

    returning follow requests on get

--- src/Zazz.Web/Controllers/Api/FollowRequestsController.cs
+++ src/Zazz.Web/Controllers/Api/FollowRequestsController.cs
@@ -4,18 +4,42 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Zazz.Core.Interfaces;
 using Zazz.Web.Filters;
 using Zazz.Web.Models.Api;
 
 namespace Zazz.Web.Controllers.Api
 {
     [HMACAuthorize]
-    public class FollowRequestsController : ApiController
+    public class FollowRequestsController : BaseApiController
     {
+        private readonly IFollowService _followService;
+        private readonly IUserService _userService;
+        private readonly IPhotoService _photoService;
+
+        public FollowRequestsController(IFollowService followService, IUserService userService,
+            IPhotoService photoService)
+        {
+            _followService = followService;
+            _userService = userService;
+            _photoService = photoService;
+        }
+
         // GET api/v1/followrequests
         public IEnumerable<ApiFollowRequest> Get()
         {
-            throw new NotImplementedException();
+            var userId = ExtractUserIdFromHeader();
+            var requests = _followService.GetFollowRequests(userId)
+                                         .Select(r => new {fromUserId = r.FromUserId, time = r.RequestDate})
+                                         .ToList();
+
+            return requests.Select(r => new ApiFollowRequest
+                                        {
+                                            UserId = r.fromUserId,
+                                            DisplayName = _userService.GetUserDisplayName(r.fromUserId),
+                                            DisplayPhoto = _photoService.GetUserImageUrl(r.fromUserId),
+                                            Time = r.time
+                                        });
         }
 
         // DELETE api/v1/followrequests/5?action=accept/reject
--- tests/Zazz.UnitTests/Web/Controllers/Api/FollowRequestsControllerShould.cs
+++ tests/Zazz.UnitTests/Web/Controllers/Api/FollowRequestsControllerShould.cs
@@ -1,6 +1,10 @@
-﻿using Moq;
+﻿using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
 
 namespace Zazz.UnitTests.Web.Controllers.Api
 {
@@ -22,6 +26,24 @@ namespace Zazz.UnitTests.Web.Controllers.Api
             {
                 x.For<IFollowService>().Use(_followService.Object);
             });
+        }
+
+        [Test]
+        public async Task ReturnFollowRequests_OnGet()
+        {
+            //Arrange
+            _followService.Setup(x => x.GetFollowRequests(User.Id))
+                          .Returns(new EnumerableQuery<FollowRequest>(Enumerable.Empty<FollowRequest>()));
+
+            AddValidHMACHeaders("GET");
+            SetupMocksForHMACAuth();
+
+            //Act
+            var result = await Client.GetAsync(ControllerAddress);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            MockRepo.VerifyAll();
         }
     }
 }
