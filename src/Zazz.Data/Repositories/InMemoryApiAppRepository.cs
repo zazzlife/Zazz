3c935aa6390e1321e058988b94f946a0f587a8b6
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-06 17:22:49 +0430

    updated app api keys

--- src/Zazz.Data/Repositories/InMemoryApiAppRepository.cs
+++ src/Zazz.Data/Repositories/InMemoryApiAppRepository.cs
@@ -13,24 +13,28 @@ namespace Zazz.Data.Repositories
         // this class is registered as a singleton, if later on you added a dependency remove the singleton flag.
         public InMemoryApiAppRepository()
         {
-            const string IOS_KEY = "7jFgrLKqfIO9YEkTqEZZQ+yVnHLZK2jm2kQEtQrpcjGIaRmQ0xCrdB9PCNJ/nbwUThaNC5ZSQi2cVcs2e232eDrz67xcmWL2qpVwuTN3zpBOIaka0FNkCinjN6SF5sZQUB/gga9KFJYzFNCe8k4nHteblxVZCujmB5ohKQYwB5k=";
+            const string IOS_REQUEST_KEY = "LGUvU+OgIcHGrrxxUedSSNY9FWKdsQ4ulRPqPzqJy9fL9jXlYX3Gq5JJvhsPloliNuOvaX12aIFJGA7LdTk6AZII0LReo0sKeft1WRBwqhmBkB/E7nFaCMQbPx1YRS5p/A1nrZAmxqGprgW0uWTuiDgULjUn9W9uUObPOCKMdK0=";
 
-            const string ANDROID_KEY = "IwLhcyZh31T1pfyFRPCRo3SIWhgufosBUzED7Kan25CM6arrZPHWHszgaF+9Z43OrEfNCQ7INBsp6YXCE0dvBEWLvZOIJJOifEo4cOHwEpBJ9PnqVV32ebXySbKoCGKwafLGMdD1uFgmmQQ6altMwvFeid2RX1u0rM/rNwEUMhI=";
+            const string IOS_PASSWORD_KEY = "0m7m7A7pqpcx8+Cw7Gq0wBtZ0DiYLxer3QFBVE6klN7WL6WXw3OedVrQXtt8BI6l0ZKVhySSzglPtjSrKCUVyk2ijZgzD+FsnDmLM0E8nmHOy0nEmm94/rcwCqW5paqwlXdX+L5v5lL2HRf7uExVeRCx6MrJG321GdzQenrzxc0=";
+
+            const string ANDROID_REQUEST_KEY = "6QrDrT+mdEWVvkSD9RZNL1hguvZfVb1oXnoGv3K9eKXu0HBWJSNDJYULhJy6LDpLoSZFPgO7alfSJs8QQiKD29zyGaT39t2ctAzmD2NnK7k4VDeuVkquk+/5TimcQSxpfa/AHPOcRXQkBUxdcMXQkwa5uL/uPbUDKJQsCCGrWGI=";
+
+            const string ANDROID_PASSWORD_KEY = "QeXvgcuD83B0RLx8axeL5nSkJXpz9+hmNJ8+ttHAR7IX5+mKJYysTjUUHS3t9SHRCBUEYQwunMPtmCuUy5Nr79WHWoDRF9trmQZb4VouImhOBV0yGr/QRkTlvx9pW73FWF2M/acP3AzWe1wUlXtYWzGjCSBaVSXbTWsqO2ym/yw=";
 
             var iosApp = new ApiApp
                          {
                              Id = 1,
                              Name = "Zazz for iOS",
-                             PasswordSigningKey = Convert.FromBase64String(IOS_KEY),
-                             RequestSigningKey = Convert.FromBase64String(IOS_KEY)
+                             PasswordSigningKey = Convert.FromBase64String(IOS_PASSWORD_KEY),
+                             RequestSigningKey = Convert.FromBase64String(IOS_REQUEST_KEY)
                          };
 
             var androidApp = new ApiApp
                              {
                                  Id = 2,
                                  Name = "Zazz for Android",
-                                 PasswordSigningKey = Convert.FromBase64String(ANDROID_KEY),
-                                 RequestSigningKey = Convert.FromBase64String(ANDROID_KEY)
+                                 PasswordSigningKey = Convert.FromBase64String(ANDROID_PASSWORD_KEY),
+                                 RequestSigningKey = Convert.FromBase64String(ANDROID_REQUEST_KEY)
                              };
 
             ApiApps.Add(iosApp);
