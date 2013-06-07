d70704c9c01df816abecd0a407e088575748d337
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-07 12:02:51 +0430

    finished add photo vote

--- src/Zazz.Infrastructure/Services/VoteService.cs
+++ src/Zazz.Infrastructure/Services/VoteService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Zazz.Core.Exceptions;
 using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
 
 namespace Zazz.Infrastructure.Services
 {
@@ -21,11 +22,20 @@ namespace Zazz.Infrastructure.Services
             if (currentUserId == 0)
                 throw new ArgumentOutOfRangeException("currentUserId");
 
-            if (!_uow.PhotoRepository.Exists(photoId))
+            var photo = _uow.PhotoRepository.GetPhotoWithMinimalData(photoId);
+            if (photo == null)
                 throw new NotFoundException();
 
             if (_uow.PhotoVoteRepository.Exists(photoId, currentUserId))
                 throw new AlreadyVotedException();
+
+            var vote = new PhotoVote { PhotoId = photoId, UserId = currentUserId };
+            
+            _uow.PhotoVoteRepository.InsertGraph(vote);
+            _uow.UserReceivedVotesRepository.Increment(photo.UserId);
+
+            _uow.SaveChanges();
+
         }
 
         public void RemovePhotoVote(int photoId, int currentUserId)
--- tests/Zazz.UnitTests/Infrastructure/Services/VoteServiceShould.cs
+++ tests/Zazz.UnitTests/Infrastructure/Services/VoteServiceShould.cs
@@ -3,6 +3,8 @@ using Moq;
 using NUnit.Framework;
 using Zazz.Core.Exceptions;
 using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+using Zazz.Core.Models.Data.DTOs;
 using Zazz.Infrastructure.Services;
 
 namespace Zazz.UnitTests.Infrastructure.Services
@@ -15,6 +17,7 @@ namespace Zazz.UnitTests.Infrastructure.Services
         private VoteService _sut;
         private int _photoId;
         private int _currentUserId;
+        private PhotoMinimalDTO _photo;
 
         [SetUp]
         public void Init()
@@ -22,6 +25,12 @@ namespace Zazz.UnitTests.Infrastructure.Services
             _mockRepo = new MockRepository(MockBehavior.Strict);
             _uow = _mockRepo.Create<IUoW>();
             _sut = new VoteService(_uow.Object);
+
+            _photo = new PhotoMinimalDTO
+                     {
+                         UserId = 4566,
+                         Id = _photoId
+                     };
 
             _photoId = 12;
             _currentUserId = 13;
@@ -53,8 +62,8 @@ namespace Zazz.UnitTests.Infrastructure.Services
         public void ThrowNotFoundIfPhotoNotExists_OnAddPhotoVote()
         {
             //Arrange
-            _uow.Setup(x => x.PhotoRepository.Exists(_photoId))
-                .Returns(false);
+            _uow.Setup(x => x.PhotoRepository.GetPhotoWithMinimalData(_photoId))
+                .Returns(() => null);
 
             //Act
             Assert.Throws<NotFoundException>(() => _sut.AddPhotoVote(_photoId, _currentUserId));
@@ -67,8 +76,8 @@ namespace Zazz.UnitTests.Infrastructure.Services
         public void ThrowIfUserHasAlreadyVoted_OnAddPhotoVote()
         {
             //Arrange
-            _uow.Setup(x => x.PhotoRepository.Exists(_photoId))
-                .Returns(true);
+            _uow.Setup(x => x.PhotoRepository.GetPhotoWithMinimalData(_photoId))
+                .Returns(_photo);
 
             _uow.Setup(x => x.PhotoVoteRepository.Exists(_photoId, _currentUserId))
                 .Returns(true);
@@ -80,6 +89,28 @@ namespace Zazz.UnitTests.Infrastructure.Services
             _mockRepo.VerifyAll();
         }
 
+        [Test]
+        public void AddNewVoteRecordAndIncrementVoteCounts_OnAddPhotoVote()
+        {
+            //Arrange
+            _uow.Setup(x => x.PhotoRepository.GetPhotoWithMinimalData(_photoId))
+                .Returns(_photo);
 
+            _uow.Setup(x => x.PhotoVoteRepository.Exists(_photoId, _currentUserId))
+                .Returns(false);
+
+            _uow.Setup(x => x.PhotoVoteRepository.InsertGraph(It.Is<PhotoVote>(p => p.PhotoId == _photoId &&
+                                                                                    p.UserId == _currentUserId)));
+            
+            _uow.Setup(x => x.UserReceivedVotesRepository.Increment(_photo.UserId));
+
+            _uow.Setup(x => x.SaveChanges());
+
+            //Act
+            _sut.AddPhotoVote(_photoId, _currentUserId);
+
+            //Assert
+            _mockRepo.VerifyAll();
+        }
     }
 }
