8e31907774843534fdd6294962355e696fef8fbc
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-07 13:51:51 +0430

    working on vote script 2

--- src/Zazz.Web/Scripts/app/vote.js
+++ src/Zazz.Web/Scripts/app/vote.js
@@ -1,9 +1,25 @@
-﻿function isUserVoted(id, btn) {
+﻿function showVoteOrUnvoteBtn(id, btn) {
     
 }
 
 function getVotesCount(id) {
-    
+
+    var url = "/vote/count/" + id;
+    var votesContainer = $('#photoVotesCount-' + id);
+
+
+    $.ajax({
+        url: url,
+        cache: false,
+        error: function() {
+            toastr.error("Failed to get the votes count");
+            votesContainer.fadeOut('slow');
+        },
+        success: function(res) {
+            votesContainer.html();
+            votesContainer.text('(' + res + ')');
+        }
+    });
 }
 
 function loadVoteButtons() {
@@ -18,8 +34,10 @@ function loadVoteButtons() {
         }
         
         $self.css('opacity', '0.6');
+        var id = $self.data('id');
 
-
+        getVotesCount(id);
+        showVoteOrUnvoteBtn(id, $self);
 
     });
 
