61881afbf4c995de5a28e28765a576226d727f6c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-07 15:01:55 +0430

    finished vote script

--- src/Zazz.Web/Scripts/app/vote.js
+++ src/Zazz.Web/Scripts/app/vote.js
@@ -1,12 +1,76 @@
-﻿function showVoteOrUnvoteBtn(id, $btn) {
+﻿function showVoteBtn($btn, id) {
+    
+    var voteHtml = '<i class="icon-thumbs-up"></i> Vote';
+    var actionUrl = "/vote/add/" + id;
+    $btn.html(voteHtml);
+
+    $btn.attr('data-url', actionUrl);
+    $btn.removeClass('disabled');
+    $btn.removeAttr('disabled');
+    $btn.attr('data-isloaded', '1');
+    $btn.css('opacity', '1');
+    $btn.attr('data-action', 'add');
+}
+
+function showRemoveVoteBtn($btn, id) {
+
+    var actionUrl = "/vote/remove/" + id;
+    var removeVoteHtml = '<i class="icon-thumbs-down"></i> Remove vote';
+
+    $btn.html(removeVoteHtml);
+    
+    $btn.attr('data-url', actionUrl);
+    $btn.removeClass('disabled');
+    $btn.removeAttr('disabled');
+    $btn.attr('data-isloaded', '1');
+    $btn.css('opacity', '1');
+    $btn.attr('data-action', 'remove');
+}
+
+$(document).on('click', 'button[data-vote-btn]', function () {
+
+    var $self = $(this);
+    var isLoaded = $self.attr('data-isloaded');
+    if (!isLoaded) {
+        return;
+    }
+
+    $self.css('opacity', '0.4');
+    var url = $self.attr('data-url');
+    var id = $self.attr('data-id');
+
+    var action = $self.attr('data-action');
+
+    $.ajax({
+        url: url,
+        cache: false,
+        error: function() {
+            toastr.error("An error occured, please try again later");
+            $self.css('opacity', '1');
+        },
+        success: function () {
+            var votesContainer = $('#photoVotesCount-' + id);
+            var votesCount = parseInt(votesContainer.text()
+                .replace(')', '')
+                .replace('(', ''));
+
+            if (action == 'remove') {
+                showVoteBtn($self, id);
+                votesCount--;
+            } else {
+                showRemoveVoteBtn($self, id);
+                votesCount++;
+            }
+
+            votesContainer.text('(' + votesCount + ')');
+        }
+    });
+
+});
+
+function loadVoteAction(id, $btn) {
     
     var url = "/vote/exists/" + id;
-
-    var removeVoteHtml = '<i class="icon-thumbs-down"></i> Remove vote';
-    var removeVoteUrl = "/vote/remove/" + id;
-    var voteUrl = "/vote/add/" + id;
-
-    var actionUrl;
 
     $.ajax({
         url: url,
@@ -14,17 +78,10 @@
         success: function(res) {
             
             if (res.toLowerCase() == 'true') {
-                $btn.html(removeVoteHtml);
-                actionUrl = removeVoteUrl;
+                showRemoveVoteBtn($btn, id);
             } else {
-                actionUrl = voteUrl;
+                showVoteBtn($btn, id);
             }
-
-            $btn.attr('data-url', actionUrl);
-            $btn.removeClass('disabled');
-            $btn.removeAttr('disabled');
-            $btn.attr('data-isloaded', '1');
-            $btn.css('opacity', '1');
         }
     });
 
@@ -56,17 +113,16 @@ function loadVoteButtons() {
 
         var $self = $(this);
 
-        var isLoaded = $self.data('isloaded');
-        if (isLoaded) {
+        var isLoaded = $self.attr('data-isloaded');
+        if (isLoaded == "1") {
             return;
         }
         
         $self.css('opacity', '0.4');
-        var id = $self.data('id');
+        var id = $self.attr('data-id');
 
         getVotesCount(id);
-        showVoteOrUnvoteBtn(id, $self);
+        loadVoteAction(id, $self);
 
     });
-
 }
