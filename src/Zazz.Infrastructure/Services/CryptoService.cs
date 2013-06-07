c23b1ef52dd4d3828a33540f555d75da66db09c7
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-07 16:49:03 +0430

    changed text signing algorithm to a stronger one

--- src/Zazz.Infrastructure/Services/CryptoService.cs
+++ src/Zazz.Infrastructure/Services/CryptoService.cs
@@ -23,18 +23,15 @@ namespace Zazz.Infrastructure.Services
         /// <summary>
         /// This key should be used to sign a string to make sure it's not tampered with later.
         /// </summary>
-        private static readonly byte[] RandomSignHashSecret =
-            {
-                109, 17, 99, 216, 57, 55, 193, 244, 152, 214, 126, 37,
-                177, 169, 58, 211
-            }; //128 bit
-        //Base64: bRFj2Dk3wfSY1n4lsak60w==
+        private readonly byte[] _randomSignHashSecretBuffer;
+        private const string RANDOM_SIGN_HASH_SECRET = "NtuWQJzOgoAtRQwZkXtUedaD/rDILB2vjGYQzfWmx91xLolrVP4Gtm9aBDGaW+NHXxGFu/7pRKg8a1H0aU1bpBISrXvAQ+gS8+Qk7BxqJ+gP3qDV4wwZdrHG8d3SoJnZJ3smlXsKT+oIpPrKFfVmK71yElTJAmVvk9uSaKW6l40=";
 
         // this class is registered as a singleton, if later on you added a dependency remove the singleton flag.
         public CryptoService()
         {
             _passwordCipherKeyBuffer = Convert.FromBase64String(PASSWORD_CIPHER_KEY);
             _qrCodeKeyBuffer = Convert.FromBase64String(QRCODE_KEY);
+            _randomSignHashSecretBuffer = Convert.FromBase64String(RANDOM_SIGN_HASH_SECRET);
         }
 
         private RijndaelManaged CreateCipher(byte[] key)
@@ -107,7 +104,8 @@ namespace Zazz.Infrastructure.Services
             if (String.IsNullOrEmpty(clearText))
                 throw new ArgumentNullException("clearText");
 
-            return ComputeSHA1SignedHash(RandomSignHashSecret, clearText);
+            var textBuffer = Encoding.UTF8.GetBytes(clearText);
+            return GenerateHMACSHA512Hash(textBuffer, _randomSignHashSecretBuffer);
         }
 
         public string GenerateQRCodeToken(byte[] userPassword)
@@ -153,17 +151,6 @@ namespace Zazz.Infrastructure.Services
                 }
 
                 return key;
-            }
-        }
-
-        private string ComputeSHA1SignedHash(byte[] secretKey, string clearText)
-        {
-            using (var hmacsha1 = new HMACSHA1(secretKey))
-            {
-                var textBytes = Encoding.UTF8.GetBytes(clearText);
-                var hash = hmacsha1.ComputeHash(textBytes);
-
-                return Convert.ToBase64String(hash);
             }
         }
     }
