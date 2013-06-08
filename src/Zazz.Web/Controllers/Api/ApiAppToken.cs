eefc548e9087e5296470569b95fe5db67e603474
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-08 12:45:07 +0430

    added api app token model

---
+++ src/Zazz.Web/Controllers/Api/ApiAppToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class ApiAppToken
+    {
+        public long RequestId { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime ExpirationTime { get; set; }
+    }
+}
