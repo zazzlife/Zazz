30203e4a65a8bfb293028aaf9d3e384665c0a137
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-09 11:13:03 +0430

    added fb friend model

---
+++ src/Zazz.Core/Models/Facebook/FbFriend.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Zazz.Core.Models.Facebook
+{
+    [DataContract]
+    public class FbFriend
+    {
+        [DataMember(Name = "uid")]
+        public long Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "pic")]
+        public string Photo { get; set; }
+
+        [DataMember(Name = "can_message")]
+        public bool CanMessage { get; set; }
+    }
+}
