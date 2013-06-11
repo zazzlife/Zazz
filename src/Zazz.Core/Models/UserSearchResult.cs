f63a5211742e1f9e88d7645279a9800ce3049e2d
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-11 09:41:19 +0430

    added search result model

---
+++ src/Zazz.Core/Models/UserSearchResult.cs
@@ -0,0 +1,15 @@
+﻿using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Core.Models
+{
+    public class UserSearchResult
+    {
+        public int UserId { get; set; }
+
+        public AccountType AccountType { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public PhotoLinks DisplayPhoto { get; set; }
+    }
+}
