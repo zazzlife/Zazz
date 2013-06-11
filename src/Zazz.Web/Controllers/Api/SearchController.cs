f21559653efc6b2c935aa901cd31a92679fe14e0
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-11 10:44:25 +0430

    added api search controller

---
+++ src/Zazz.Web/Controllers/Api/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models;
+using Zazz.Web.Filters;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [HMACAuthorize]
+    public class SearchController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public SearchController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET /api/v1/search?query=
+
+        public UserSearchResult Get(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/SearchControllerShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class SearchControllerShould : BaseHMACTests
+    {
+        public override void Init()
+        {
+            base.Init();
+            var query = "soroush";
+            ControllerAddress = "/api/v1/search?query=" + query;
+
+
+            IocContainer.Configure(x =>
+            {
+            });
+        }
+    }
+}
