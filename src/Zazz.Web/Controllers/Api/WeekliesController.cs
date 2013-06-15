8342af0d301ff62474ac41c9ff354f8c3e9dd63b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-15 16:44:11 +0430

    added weeklies controller

---
+++ src/Zazz.Web/Controllers/Api/WeekliesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Web.Filters;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [HMACAuthorize]
+    public class WeekliesController : BaseApiController
+    {
+        private readonly IWeeklyService _weeklyService;
+
+        public WeekliesController(IWeeklyService weeklyService)
+        {
+            _weeklyService = weeklyService;
+        }
+
+        // GET api/v1/weeklies/5
+        public ApiWeekly Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/v1/weeklies
+        public ApiWeekly Post(ApiWeekly weekly)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Put api/v1/weeklies/5
+        public void Put(int id, ApiWeekly weekly)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Delete api/v1/weeklies/5
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/WeekliesControllerShould.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using Zazz.Core.Interfaces;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class WeekliesControllerShould : BaseHMACTests
+    {
+        private int _weeklyId;
+        private Mock<IWeeklyService> _weeklyService;
+
+        public override void Init()
+        {
+            base.Init();
+            
+            _weeklyId = 21;
+            ControllerAddress = "/api/v1/weeklies/" + _weeklyId;
+
+            _weeklyService = MockRepo.Create<IWeeklyService>();
+            IocContainer.Configure(x =>
+                                   {
+                                       x.For<IWeeklyService>().Use(_weeklyService.Object);
+                                   });
+        }
+    }
+}
