fb072626d5b2e9c92790b0aceb60d2a63fb4b1da
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-19 13:56:17 +0430

    added tag stats cache

---
+++ src/Zazz.Core/Interfaces/ITagStatsCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface ITagStatsCache
+    {
+        DateTime LastUpdate { get; set; }
+
+        IList<TagStat> TagStats { get; set; }
+    }
+}
---
+++ src/Zazz.Infrastructure/TagStatsCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models.Data;
+
+namespace Zazz.Infrastructure
+{
+    //This class is a singleton
+    public class TagStatsCache : ITagStatsCache
+    {
+        public DateTime LastUpdate { get; set; }
+        public IList<TagStat> TagStats { get; set; }
+    }
+}
