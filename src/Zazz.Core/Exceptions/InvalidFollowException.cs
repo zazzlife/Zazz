062204d5de4a0c7731dea4fc6065f722ee3826d4
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-06-29 14:29:51 +0430

    added new exception type

---
+++ src/Zazz.Core/Exceptions/InvalidFollowException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Zazz.Core.Exceptions
+{
+    [Serializable]
+    public class InvalidFollowException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public InvalidFollowException()
+        { }
+
+        public InvalidFollowException(string message)
+            : base(message)
+        { }
+
+        public InvalidFollowException(string message, Exception inner)
+            : base(message, inner)
+        { }
+
+        protected InvalidFollowException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        { }
+    }
+}
