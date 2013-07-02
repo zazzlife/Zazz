dce13740daf658ff35a9d69443848bafc400f585
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-02 13:12:52 +0430

    added token controller

---
+++ src/Zazz.Web/Controllers/Api/TokenController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class TokenController : ApiController
+    {
+        public OAuthAccessTokenResponse Post(OAuthAccessTokenRequest request)
+        {
+            
+        }
+    }
+
+    public class OAuthAccessTokenRequest
+    {}
+
+    public class OAuthAccessTokenResponse
+    {}
+}
---
+++ tests/Zazz.UnitTests/Web/Controllers/Api/TokenControllerShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class TokenControllerShould
+    {
+        [SetUp]
+        public void Init()
+        {
+            
+        }
+    }
+}
