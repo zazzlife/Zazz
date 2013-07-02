cde5b2e2e1fac27d92c51ad69c6b0995a260ebc7
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-02 19:15:18 +0430

    working on jwt

---
+++ src/Zazz.Web/JWT/JsonWebToken.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zazz.Web.JWT
+{
+    public class JsonWebToken
+    {
+        public JWTHeader Header { get; set; }
+
+        public ICollection<KeyValuePair<string, object>> Claims { get; set; }
+
+        public string Signature { get; set; }
+    }
+
+    public class JWTHeader
+    {
+        /// <summary>
+        /// Issuer
+        /// </summary>
+        public string iss { get; set; }
+
+        /// <summary>
+        /// Audience
+        /// </summary>
+        public string aud { get; set; }
+
+        /// <summary>
+        /// Not Before
+        /// </summary>
+        public long nbf { get; set; }
+
+        /// <summary>
+        /// Expiration Time
+        /// </summary>
+        public long exp { get; set; }
+
+        /// <summary>
+        /// Type
+        /// </summary>
+        private string typ { get { return "JWT"; } }
+
+        /// <summary>
+        /// Algorithm
+        /// </summary>
+        public string alg { get; set; }
+    }
+}
