70cbf21119fb4d65d8152705fda4fe5129f9d3d1
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-02 12:57:59 +0430

    added oauth 2 authorize attribute

---
+++ src/Zazz.Web/Filters/OAuth2AuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Zazz.Web.Filters
+{
+    public class OAuth2AuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            return base.IsAuthorized(actionContext);
+        }
+
+        protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
+        {
+            base.HandleUnauthorizedRequest(actionContext);
+        }
+    }
+}
