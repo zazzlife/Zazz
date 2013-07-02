b024a8f6975f8ead17f64e07e6cdfe7f84862d9c
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-02 19:25:50 +0430

    added jwt handler

---
+++ src/Zazz.Web/JWT/JsonWebTokenHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Zazz.Web.JWT
+{
+    public class JsonWebTokenHandler
+    {
+        private readonly byte[] _key;
+
+        public JsonWebTokenHandler(byte[] key)
+        {
+            _key = key;
+        }
+
+        public JsonWebTokenHandler(string key)
+        {
+            _key = Convert.FromBase64String(key);
+        }
+
+        public string Encode(HashSet<KeyValuePair<string, object>> claims, DateTime? expirationData = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonWebToken Decode(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
