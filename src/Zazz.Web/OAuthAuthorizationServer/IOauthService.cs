4ba3d9ceedb8938d18e858b6ebadeaea9c8bead4
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-04 15:59:52 +0430

    added oauth service

---
+++ src/Zazz.Web/OAuthAuthorizationServer/IOauthService.cs
@@ -0,0 +1,12 @@
+﻿using Zazz.Core.Models.Data;
+using Zazz.Web.OAuthAuthorizationServer.JsonWebToken;
+
+namespace Zazz.Web.OAuthAuthorizationServer
+{
+    public interface IOAuthService
+    {
+        OAuthCredentials CreateOAuthCredentials(User user);
+
+        JWT RefreshAccessToken(string refreshToken);
+    }
+}
