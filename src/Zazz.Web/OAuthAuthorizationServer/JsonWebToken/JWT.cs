026ab66006a1512d9e939f2d993dbe5df4f7ea40
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-04 13:11:59 +0430

    moving JWTHandler functions to JWT class itself 1

--- src/Zazz.Web/OAuthAuthorizationServer/JsonWebToken/JWT.cs
+++ src/Zazz.Web/OAuthAuthorizationServer/JsonWebToken/JWT.cs
@@ -15,6 +15,27 @@ namespace Zazz.Web.OAuthAuthorizationServer.JsonWebToken
         public DateTime? ExpirationTime { get; set; }
 
         public string Signature { get; set; }
+
+        public JWT()
+        {}
+
+        /// <summary>
+        /// Decodes an existing JWT string and populates the properties
+        /// </summary>
+        /// <param name="jwtString"></param>
+        public JWT(string jwtString)
+        {
+            
+        }
+
+        /// <summary>
+        /// Returns a JWT encoded string based on properties
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class JWTHeader
