b21c8dee459b973b6eb9d5c17415453d84ff54b4
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-08 13:50:09 +0430

    added club reward service interface

---
+++ src/Zazz.Core/Interfaces/IClubRewardService.cs
@@ -0,0 +1,25 @@
+﻿using Zazz.Core.Models.Data;
+
+namespace Zazz.Core.Interfaces
+{
+    public interface IClubRewardService
+    {
+        void AddRewardScenario(ClubPointRewardScenario scenario);
+
+        void ChangeRewardAmount(int scenarioId, int currentUserId, int amount);
+
+        void RemoveRewardScenario(int scenarioId, int currentUserId);
+
+
+        void AddClubReward(ClubReward reward);
+
+        void UpdateClubReward(int rewardId, int currentUserId, ClubReward newReward);
+
+        void RemoveClubReward(int rewardId, int currentUserId);
+
+
+        UserPoint RewardUserPoints(int clubId, int userId, int amount);
+
+        UserReward RedeemPoints(int userId, ClubReward reward);
+    }
+}
