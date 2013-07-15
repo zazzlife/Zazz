22faefb7078cc397e2d33a9cd9b4099e070fc262
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-15 15:53:15 +0430

    checking username availability. closes #42

--- src/Zazz.Web/Scripts/app/register.js
+++ src/Zazz.Web/Scripts/app/register.js
@@ -5,3 +5,60 @@
 $('#_AccountType_ClubAdmin').click(function () {
     $('#clubDetails').slideDown('fast');
 });
+
+function isUsernameAvailable(username) {
+
+    var url = "/account/isAvailable";
+    var dff = $.Deferred();
+
+    $.ajax({
+        url: url,
+        data: {
+            username: username
+        },
+        success: function(res) {
+
+            if (res.toLowerCase() == "false") {
+                dff.resolve(false);
+            } else {
+                dff.resolve(true);
+            }
+        }
+    });
+
+    return dff.promise();
+}
+
+$(document).on('change', 'input[data-checkusername]', function(e) {
+
+    var self = $(this);
+    var username = self.val();
+    if (!username) {
+        return;
+    }
+
+    var container = self.closest('.control-group');
+    var help = container.find('.help-inline');
+    
+    var usrNamePromise = isUsernameAvailable(username);
+
+    usrNamePromise.done(function(isAvailable) {
+
+        if (isAvailable) {
+            container.removeClass('error');
+            container.addClass('success');
+
+            help.text('Available!');
+
+        } else {
+            container.removeClass('success');
+            container.addClass('error');
+
+            help.text('Username is not available');
+
+        }
+        
+
+    });
+
+})
