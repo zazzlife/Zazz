4173753c26d89963fd9b14b761ac2af2d9bc862a
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-15 10:39:50 +0430

    added api register model

---
+++ src/Zazz.Web/Models/Api/ApiRegister.cs
@@ -0,0 +1,25 @@
+﻿using Zazz.Core.Models.Data.Enums;
+
+namespace Zazz.Web.Models.Api
+{
+    public class ApiRegister
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string FullName { get; set; }
+
+        public AccountType AccountType { get; set; }
+
+        public string ClubName { get; set; }
+
+        public ClubType ClubType { get; set; }
+
+        public string ClubAddress { get; set; }
+    }
+}
