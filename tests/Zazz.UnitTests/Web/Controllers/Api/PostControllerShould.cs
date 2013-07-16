f9e96c96609b31c55e8c9f2025cfe3896e0897bc
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-16 12:26:15 +0430

    posts controller unit tests

--- tests/Zazz.UnitTests/Web/Controllers/Api/PostControllerShould.cs
+++ tests/Zazz.UnitTests/Web/Controllers/Api/PostControllerShould.cs
@@ -1,257 +1,247 @@
-﻿//using System;
-//using System.Net;
-//using System.Net.Http;
-//using System.Security;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Web.Http.Filters;
-//using System.Web.Http.SelfHost;
-//using Moq;
-//using NUnit.Framework;
-//using Newtonsoft.Json;
-//using StructureMap;
-//using Zazz.Core.Exceptions;
-//using Zazz.Core.Interfaces;
-//using Zazz.Core.Models;
-//using Zazz.Core.Models.Data;
-//using Zazz.Infrastructure.Services;
-//using Zazz.Web;
-//using Zazz.Web.DependencyResolution;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using System.Web.Http.SelfHost;
+using Moq;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using StructureMap;
+using Zazz.Core.Exceptions;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Models;
+using Zazz.Core.Models.Data;
+using Zazz.Infrastructure.Services;
+using Zazz.Web;
+using Zazz.Web.DependencyResolution;
 
-//namespace Zazz.UnitTests.Web.Controllers.Api
-//{
-//    [TestFixture]
-//    public class PostControllerShould : BaseHMACTests
-//    {
-//        private Mock<IPostService> _postService;
-//        private Post _post;
-//        private int _postId;
+namespace Zazz.UnitTests.Web.Controllers.Api
+{
+    [TestFixture]
+    public class PostControllerShould : BaseOAuthTests
+    {
+        private Mock<IPostService> _postService;
+        private Post _post;
+        private int _postId;
 
-//        public override void Init()
-//        {
-//            base.Init();
-            
-//            _postId = 99;
-//            ControllerAddress = "/api/v1/posts/" + _postId;
+        public override void Init()
+        {
+            base.Init();
 
-//            _post = new Post
-//                    {
-//                        Id = _postId,
-//                        FromUserId = User.Id,
-//                        CreatedTime = DateTime.UtcNow,
-//                        Message = "Message"
-//                    };
+            _postId = 99;
+            ControllerAddress = "/api/v1/posts/" + _postId;
 
-//            _postService = MockRepo.Create<IPostService>();
-//            IocContainer.Configure(x =>
-//                                   {
-//                                       x.For<IPostService>().Use(_postService.Object);
-//                                   });
-//        }
+            _post = new Post
+                    {
+                        Id = _postId,
+                        FromUserId = User.Id,
+                        CreatedTime = DateTime.UtcNow,
+                        Message = "Message"
+                    };
 
-//        [Test]
-//        public async Task Return404IfPostIsNotFound_OnGet()
-//        {
-//            //Arrange
-//            AddValidHMACHeaders("GET");
-//            SetupMocksForHMACAuth();
+            _postService = MockRepo.Create<IPostService>();
+            IocContainer.Configure(x =>
+                                   {
+                                       x.For<IPostService>().Use(_postService.Object);
+                                   });
+        }
 
-//            _postService.Setup(x => x.GetPost(_postId))
-//                        .Returns(() => null);
+        [Test]
+        public async Task Return404IfPostIsNotFound_OnGet()
+        {
+            //Arrange
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.GetAsync(ControllerAddress);
+            _postService.Setup(x => x.GetPost(_postId))
+                        .Returns(() => null);
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.GetAsync(ControllerAddress);
 
-//        [Test]
-//        public async Task Return200WhenEverythingIsOK_OnGet()
-//        {
-//            //Arrange
-//            AddValidHMACHeaders("GET");
-//            SetupMocksForHMACAuth();
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            _postService.Setup(x => x.GetPost(_postId))
-//                        .Returns(_post);
+        [Test]
+        public async Task Return200WhenEverythingIsOK_OnGet()
+        {
+            //Arrange
+            CreateValidAccessToken();
 
-//            UserService.Setup(x => x.GetUserDisplayName(_post.FromUserId))
-//                       .Returns("display name");
-//            PhotoService.Setup(x => x.GetUserImageUrl(_post.FromUserId))
-//                        .Returns(new PhotoLinks("url"));
+            _postService.Setup(x => x.GetPost(_postId))
+                        .Returns(_post);
 
-//            //Act
-//            var response = await Client.GetAsync(ControllerAddress);
+            UserService.Setup(x => x.GetUserDisplayName(_post.FromUserId))
+                       .Returns("display name");
+            PhotoService.Setup(x => x.GetUserImageUrl(_post.FromUserId))
+                        .Returns(new PhotoLinks("url"));
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.GetAsync(ControllerAddress);
 
-//        [TestCase(null)]
-//        [TestCase("")]
-//        [TestCase(" ")]
-//        public async Task Return400IfPostMessageIsMissing_OnPost(string message)
-//        {
-//            //Arrange
-//            _post.Message = message;
-//            ControllerAddress = "/api/v1/posts";
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            var json = JsonConvert.SerializeObject(_post);
-//            var postContent = new StringContent(json, Encoding.UTF8, "application/json");
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public async Task Return400IfPostMessageIsMissing_OnPost(string message)
+        {
+            //Arrange
+            _post.Message = message;
+            ControllerAddress = "/api/v1/posts";
 
-//            AddValidHMACHeaders("POST", ControllerAddress, json);
-//            SetupMocksForHMACAuth();
+            var json = JsonConvert.SerializeObject(_post);
+            var postContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-//            //Act
-//            var response = await Client.PostAsync(ControllerAddress, postContent);
+            CreateValidAccessToken();
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.PostAsync(ControllerAddress, postContent);
 
-//        [Test]
-//        public async Task AssignTheCurrentUserToPostAndSaveIt_OnPost()
-//        {
-//            //Arrange
-//            _post.FromUserId = User.Id + 1;
-//            ControllerAddress = "/api/v1/posts";
+            //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            var json = JsonConvert.SerializeObject(_post);
-//            var postContent = new StringContent(json, Encoding.UTF8, "application/json");
+        [Test]
+        public async Task AssignTheCurrentUserToPostAndSaveIt_OnPost()
+        {
+            //Arrange
+            _post.FromUserId = User.Id + 1;
+            ControllerAddress = "/api/v1/posts";
 
-//            AddValidHMACHeaders("POST", ControllerAddress, json);
-//            SetupMocksForHMACAuth();
+            var json = JsonConvert.SerializeObject(_post);
+            var postContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-//            _postService.Setup(x => x.NewPost(It.Is<Post>(p => p.FromUserId == User.Id)));
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.PostAsync(ControllerAddress, postContent);
+            _postService.Setup(x => x.NewPost(It.Is<Post>(p => p.FromUserId == User.Id)));
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.PostAsync(ControllerAddress, postContent);
 
-//        [TestCase(null)]
-//        [TestCase("")]
-//        [TestCase(" ")]
-//        public async Task Return400IfPostMessageIsEmpty_OnPut(string message)
-//        {
-//            //Arrange
-//            _post.Message = message;
-//            var json = JsonConvert.SerializeObject(_post);
-//            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            AddValidHMACHeaders("PUT", ControllerAddress, json);
-//            SetupMocksForHMACAuth();
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public async Task Return400IfPostMessageIsEmpty_OnPut(string message)
+        {
+            //Arrange
+            _post.Message = message;
+            var json = JsonConvert.SerializeObject(_post);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-//            //Act
-//            var response = await Client.PutAsync(ControllerAddress, httpContent);
+            CreateValidAccessToken();
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.PutAsync(ControllerAddress, httpContent);
 
-//        [Test]
-//        public async Task Return404ForNotFoundException_OnPut()
-//        {
-//            //Arrange
-//            var json = JsonConvert.SerializeObject(_post);
-//            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            AddValidHMACHeaders("PUT", ControllerAddress, json);
-//            SetupMocksForHMACAuth();
+        [Test]
+        public async Task Return404ForNotFoundException_OnPut()
+        {
+            //Arrange
+            var json = JsonConvert.SerializeObject(_post);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-//            _postService.Setup(x => x.EditPost(_postId, _post.Message, User.Id))
-//                        .Throws<NotFoundException>();
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.PutAsync(ControllerAddress, httpContent);
+            _postService.Setup(x => x.EditPost(_postId, _post.Message, User.Id))
+                        .Throws<NotFoundException>();
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.PutAsync(ControllerAddress, httpContent);
 
-//        [Test]
-//        public async Task Return403ForSecurityException_OnPut()
-//        {
-//            //Arrange
-//            var json = JsonConvert.SerializeObject(_post);
-//            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            AddValidHMACHeaders("PUT", ControllerAddress, json);
-//            SetupMocksForHMACAuth();
+        [Test]
+        public async Task Return403ForSecurityException_OnPut()
+        {
+            //Arrange
+            var json = JsonConvert.SerializeObject(_post);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-//            _postService.Setup(x => x.EditPost(_postId, _post.Message, User.Id))
-//                        .Throws<SecurityException>();
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.PutAsync(ControllerAddress, httpContent);
+            _postService.Setup(x => x.EditPost(_postId, _post.Message, User.Id))
+                        .Throws<SecurityException>();
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.PutAsync(ControllerAddress, httpContent);
 
-//        [Test]
-//        public async Task Return204IfSuccessful_OnPut()
-//        {
-//            //Arrange
-//            var json = JsonConvert.SerializeObject(_post);
-//            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            AddValidHMACHeaders("PUT", ControllerAddress, json);
-//            SetupMocksForHMACAuth();
+        [Test]
+        public async Task Return204IfSuccessful_OnPut()
+        {
+            //Arrange
+            var json = JsonConvert.SerializeObject(_post);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-//            _postService.Setup(x => x.EditPost(_postId, _post.Message, User.Id));
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.PutAsync(ControllerAddress, httpContent);
+            _postService.Setup(x => x.EditPost(_postId, _post.Message, User.Id));
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.PutAsync(ControllerAddress, httpContent);
 
-//        [Test]
-//        public async Task Return403ForSecurityException_OnDelete()
-//        {
-//            //Arrange
-//            AddValidHMACHeaders("DELETE", ControllerAddress);
-//            SetupMocksForHMACAuth();
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            _postService.Setup(x => x.RemovePost(_postId, User.Id))
-//                        .Throws<SecurityException>();
+        [Test]
+        public async Task Return403ForSecurityException_OnDelete()
+        {
+            //Arrange
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.DeleteAsync(ControllerAddress);
+            _postService.Setup(x => x.RemovePost(_postId, User.Id))
+                        .Throws<SecurityException>();
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
+            //Act
+            var response = await Client.DeleteAsync(ControllerAddress);
 
-//        [Test]
-//        public async Task Return204IfSuccessful_OnDelete()
-//        {
-//            //Arrange
-//            AddValidHMACHeaders("DELETE", ControllerAddress);
-//            SetupMocksForHMACAuth();
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
 
-//            _postService.Setup(x => x.RemovePost(_postId, User.Id));
+        [Test]
+        public async Task Return204IfSuccessful_OnDelete()
+        {
+            //Arrange
+            CreateValidAccessToken();
 
-//            //Act
-//            var response = await Client.DeleteAsync(ControllerAddress);
+            _postService.Setup(x => x.RemovePost(_postId, User.Id));
 
-//            //Assert
-//            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
-//            MockRepo.VerifyAll();
-//        }
-//    }
-//}
+            //Act
+            var response = await Client.DeleteAsync(ControllerAddress);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+            MockRepo.VerifyAll();
+        }
+    }
+}
