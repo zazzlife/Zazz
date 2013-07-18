cabb1e104e063de15834d072d833174fc11b2688
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-18 16:20:50 +0430

    added new controller

---
+++ src/Zazz.Web/Controllers/Api/MeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Web.Filters;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [OAuth2Authorize]
+    public class MeController : BaseApiController
+    {
+        private readonly IUserService _userService;
+        private readonly IPhotoService _photoService;
+
+        public MeController(IUserService userService, IPhotoService photoService)
+        {
+            _userService = userService;
+            _photoService = photoService;
+        }
+
+        public ApiBasicUserInfo Get()
+        {
+            var user = _userService.GetUser(CurrentUserId);
+
+            return new ApiBasicUserInfo
+                   {
+                       AccountType = user.AccountType,
+                       DisplayName = _userService.GetUserDisplayName(user.Id),
+                       DisplayPhoto = _photoService.GetUserImageUrl(user.Id),
+                       IsConfirmed = user.IsConfirmed,
+                       UserId = user.Id
+                   };
+        }
+
+    }
+
+
+}
