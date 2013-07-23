764c9aad9772729e1c2242f3258a8bb9d44dbe97
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-23 11:37:41 +0430

    applied photo attribute to photo links

--- src/Zazz.Core/Models/PhotoLinks.cs
+++ src/Zazz.Core/Models/PhotoLinks.cs
@@ -1,25 +1,31 @@
-﻿namespace Zazz.Core.Models
+﻿using Zazz.Core.Attributes;
+
+namespace Zazz.Core.Models
 {
     public class PhotoLinks
     {
         /// <summary>
         /// This one is for user image on comments and feeds and like that. It's around 55px
         /// </summary>
+        [Photo(Height = 55, Width = 55, Quality = 95, Suffix = "vs")]
         public string VerySmallLink { get; set; }
 
         /// <summary>
         /// This one is for images on feeds and photo lists and like that. It's around 175px
         /// </summary>
+        [Photo(Height = 175, Width = 175, Quality = 85, Suffix = "s")]
         public string SmallLink { get; set; }
 
         /// <summary>
         /// This one is for images on feeds and like that. It's around 500px
         /// </summary>
+        [Photo(Height = 500, Width = 500, Quality = 70, Suffix = "m")]
         public string MediumLink { get; set; }
 
         /// <summary>
         /// This one is for when you show the image in full size, maximum size is around 1600px
         /// </summary>
+        [Photo(Height = 1600, Width = 1600, Quality = 60)]
         public string OriginalLink { get; set; }
 
         public PhotoLinks()
