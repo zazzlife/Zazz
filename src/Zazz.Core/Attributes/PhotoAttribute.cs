bc64949387d01c90a7aba861f2933e313438bbf0
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-23 11:32:31 +0430

    added a new photo attribute

---
+++ src/Zazz.Core/Attributes/PhotoAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zazz.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PhotoAttribute : Attribute
+    {
+        public string Suffix { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public long Quality { get; set; }
+
+    }
+}
