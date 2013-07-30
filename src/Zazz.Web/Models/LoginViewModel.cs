b96952766a7e7ce363ee2b461653fb9ef545b920
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-30 12:54:40 +0430

    added username regex to login page

--- src/Zazz.Web/Models/LoginViewModel.cs
+++ src/Zazz.Web/Models/LoginViewModel.cs
@@ -8,6 +8,7 @@ namespace Zazz.Web.Models
         [Required]
         [Display(Name = "Username")]
         [StringLength(20, MinimumLength = 2, ErrorMessage = "{0} must be between {2} and {1} characters.")]
+        [RegularExpression(@"^([a-zA-Z0-9._]+)$", ErrorMessage = "{0} contains invalid character(s)")]
         public string Username { get; set; }
 
         [Required, DataType(DataType.Password), AllowHtml]
