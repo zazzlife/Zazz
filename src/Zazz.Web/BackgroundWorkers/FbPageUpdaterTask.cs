20b9ff438d301a36518709a905184b661c5fa22b
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-07-31 11:39:49 +0430

    created a new background task for updating fb pages

---
+++ src/Zazz.Web/BackgroundWorkers/FbPageUpdaterTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using FluentScheduler;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Interfaces.Services;
+
+namespace Zazz.Web.BackgroundWorkers
+{
+    public class FbPageUpdaterTask : ITask
+    {
+        private readonly IFacebookService _facebookService;
+        private readonly IUoW _uow;
+
+        public FbPageUpdaterTask(IFacebookService facebookService, IUoW uow)
+        {
+            _facebookService = facebookService;
+            _uow = uow;
+        }
+
+        public void Execute()
+        {
+            // NOTE: don't use this method if the pages grow over 1000
+            var pages = _uow.FacebookPageRepository
+                .GetAll()
+                .Select(p => p.FacebookId)
+                .ToList();
+
+            foreach (var page in pages)
+            {
+                // not sending them in parallel so facebook won't block us!
+                _facebookService.UpdatePageEvents(page);
+                _facebookService.UpdatePagePhotos(page);
+                _facebookService.UpdatePageStatuses(page);
+
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
