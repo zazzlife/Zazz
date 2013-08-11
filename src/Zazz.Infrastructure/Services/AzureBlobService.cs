a8fab59d735a8bf827a727cad134d648d10d6bcf
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-08-11 16:53:55 +0430

    azure blob storage - base url

--- src/Zazz.Infrastructure/Services/AzureBlobService.cs
+++ src/Zazz.Infrastructure/Services/AzureBlobService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Zazz.Core.Interfaces;
@@ -9,18 +10,21 @@ namespace Zazz.Infrastructure.Services
     public class AzureBlobService : IStorageService
     {
         private readonly string _storageConnString;
+        private readonly string _storageAccountName;
 
         private const string PIC_CONTAINER_NAME = "pictures";
 
+        //http://zazz.blob.core.windows.net/pictures
         public string BasePhotoUrl
         {
-            get { return "TODO: RETURN BLOB URL"; }
+            get { return String.Format("http://{0}.blob.core.windows.net/{1}", _storageAccountName, PIC_CONTAINER_NAME); }
         }
 
         // this class is registered as a singleton, if later on you added a dependency remove the singleton flag.
-        public AzureBlobService(string storageConnString) 
+        public AzureBlobService(string storageConnString, string storageAccountName)
         {
             _storageConnString = storageConnString;
+            _storageAccountName = storageAccountName;
         }
 
         public void SavePhotoBlob(string fileName, Stream data)
