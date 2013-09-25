0436ca13832f123dfad07640f07f212719208fdc
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-09-25 12:52:04 +0330

    script fix

--- src/Zazz.Web/OtherScripts/gallery.js
+++ src/Zazz.Web/OtherScripts/gallery.js
@@ -755,7 +755,7 @@ $(function () {
 			init = function () {
 
 			    // (not necessary) preloading the images here...
-			    $items.add('<img src="images/ajax-loader.gif"/><img src="images/black.png"/>').imagesLoaded(function () {
+			    $items.add('<img src="/Images/ajax-loader.gif"/><img src="/Images/black.png"/>').imagesLoaded(function () {
 			        // add options
 			        _addViewModes();
 
