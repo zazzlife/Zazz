f0167f5272849ffcea33e393c2c4c882a86da8d8
Soroush Mirzaei <Soroush.Mirzaei@gmail.com>
2013-10-01 12:27:51 +0330

    commented event scripts (flip and show larger pic)

--- src/Zazz.Web/Scripts/app/event.js
+++ src/Zazz.Web/Scripts/app/event.js
@@ -1,134 +1,134 @@
-﻿var eventLargeImg;
+﻿//var eventLargeImg;
 
-$(document).on('mouseenter', '.event', function () {
-    var self = $(this);
+//$(document).on('mouseenter', '.event', function () {
+//    var self = $(this);
 
-    var img = self.data('img');
-    if (!img) {
-        return;
-    }
+//    var img = self.data('img');
+//    if (!img) {
+//        return;
+//    }
 
-    var imgMaxHeight = 300;
-    var offset = self.offset();
-    var elemWidth = self.width();
-    var elemHeight = self.height();
-    var top, left;
-    var side = self.parent().parent().attr('data-eventImgSide');
+//    var imgMaxHeight = 300;
+//    var offset = self.offset();
+//    var elemWidth = self.width();
+//    var elemHeight = self.height();
+//    var top, left;
+//    var side = self.parent().parent().attr('data-eventImgSide');
     
-    var centerY = offset.top + (elemHeight / 2);
+//    var centerY = offset.top + (elemHeight / 2);
 
-    eventLargeImg = $('<img />', {
-        src: img,
-    });
+//    eventLargeImg = $('<img />', {
+//        src: img,
+//    });
 
-    eventLargeImg.css({
-        'position': 'absolute',
-        'max-height': imgMaxHeight + 'px'
-    });
+//    eventLargeImg.css({
+//        'position': 'absolute',
+//        'max-height': imgMaxHeight + 'px'
+//    });
 
-    eventLargeImg.appendTo($('article')).hide();
+//    eventLargeImg.appendTo($('article')).hide();
 
-    eventLargeImg.ensureLoad(function () {
+//    eventLargeImg.ensureLoad(function () {
         
-        if (side == "left") {
-            self.addClass('event-arrow-left');
-            left = offset.left - eventLargeImg.width() - 15;
-        } else {
-            self.addClass('event-arrow-right');
-            left = offset.left + elemWidth + 30;
-        }
+//        if (side == "left") {
+//            self.addClass('event-arrow-left');
+//            left = offset.left - eventLargeImg.width() - 15;
+//        } else {
+//            self.addClass('event-arrow-right');
+//            left = offset.left + elemWidth + 30;
+//        }
 
-        top = (centerY - eventLargeImg.height() / 2);
+//        top = (centerY - eventLargeImg.height() / 2);
 
-        eventLargeImg.css({
-            'left': left,
-            'top': top
-        });
+//        eventLargeImg.css({
+//            'left': left,
+//            'top': top
+//        });
 
-        eventLargeImg.show();
-    });
-});
+//        eventLargeImg.show();
+//    });
+//});
 
-$(document).on('mouseleave', '.event', function () {
-    $(this).removeClass('event-arrow-right');
-    $(this).removeClass('event-arrow-left');
+//$(document).on('mouseleave', '.event', function () {
+//    $(this).removeClass('event-arrow-right');
+//    $(this).removeClass('event-arrow-left');
 
-    if (eventLargeImg) {
-        eventLargeImg.remove();
-    }
+//    if (eventLargeImg) {
+//        eventLargeImg.remove();
+//    }
     
-});
+//});
 
-/////////////////// Flip /////////////////////////
+///////////////////// Flip /////////////////////////
 
-var flipDuration = 250;
+//var flipDuration = 250;
 
-function flipToBack(elem, front, back) {
+//function flipToBack(elem, front, back) {
 
-    $(elem).transition({
-        perspective: '1000px',
-        rotateX: '90deg',
-        duration: flipDuration,
-        complete: function () {
-            front.hide();
-            back.show();
-        }
-    }).transition({
-        perspective: '1000px',
-        rotateX: '-90deg',
-        duration: 0
-    }).transition({
-        perspective: '1000px',
-        rotateX: '0deg',
-        duration: flipDuration,
-        complete: function () {
-            $(elem).data('current-side', 'back');
-        }
-    });
-}
+//    $(elem).transition({
+//        perspective: '1000px',
+//        rotateX: '90deg',
+//        duration: flipDuration,
+//        complete: function () {
+//            front.hide();
+//            back.show();
+//        }
+//    }).transition({
+//        perspective: '1000px',
+//        rotateX: '-90deg',
+//        duration: 0
+//    }).transition({
+//        perspective: '1000px',
+//        rotateX: '0deg',
+//        duration: flipDuration,
+//        complete: function () {
+//            $(elem).data('current-side', 'back');
+//        }
+//    });
+//}
 
-function flipToFront(elem, front, back) {
+//function flipToFront(elem, front, back) {
 
-    $(elem).transition({
-        perspective: '1000px',
-        rotateX: '-90deg',
-        duration: flipDuration,
-        complete: function () {
-            front.show();
-            back.hide();
-        }
-    }).transition({
-        perspective: '1000px',
-        rotateX: '90deg',
-        duration: 0
-    }).transition({
-        perspective: '1000px',
-        rotateX: '0deg',
-        duration: flipDuration,
-        complete: function () {
-            $(elem).data('current-side', 'front');
-        }
-    });
-}
+//    $(elem).transition({
+//        perspective: '1000px',
+//        rotateX: '-90deg',
+//        duration: flipDuration,
+//        complete: function () {
+//            front.show();
+//            back.hide();
+//        }
+//    }).transition({
+//        perspective: '1000px',
+//        rotateX: '90deg',
+//        duration: 0
+//    }).transition({
+//        perspective: '1000px',
+//        rotateX: '0deg',
+//        duration: flipDuration,
+//        complete: function () {
+//            $(elem).data('current-side', 'front');
+//        }
+//    });
+//}
 
-$(document).on('click', '.event', function (e) {
+//$(document).on('click', '.event', function (e) {
 
 
-    var self = this;
+//    var self = this;
 
-    var side = $(self).data('current-side');
-    var front = $(self).children('*[data-side="front"]');
-    var back = $(self).children('*[data-side="back"]');
+//    var side = $(self).data('current-side');
+//    var front = $(self).children('*[data-side="front"]');
+//    var back = $(self).children('*[data-side="back"]');
 
-    if (side == "front") {
-        flipToBack(self, front, back);
+//    if (side == "front") {
+//        flipToBack(self, front, back);
 
-    } else {
-        flipToFront(self, front, back);
-    }
+//    } else {
+//        flipToFront(self, front, back);
+//    }
 
-});
+//});
 
-$(document).on('click', '.event a', function (e) {
-    e.stopPropagation();
-});
+//$(document).on('click', '.event a', function (e) {
+//    e.stopPropagation();
+//});
