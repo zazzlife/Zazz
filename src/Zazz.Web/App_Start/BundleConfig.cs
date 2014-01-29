c007c51a1e0f86271a7f5e8cedc5cf8adcff7446
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2014-01-29 10:40:53 +0330

    added a new css file

--- src/Zazz.Web/App_Start/BundleConfig.cs
+++ src/Zazz.Web/App_Start/BundleConfig.cs
@@ -33,6 +33,8 @@ namespace Zazz.Web
             cssOrder.Files.Add("bootstrap.css");
             cssOrder.Files.Add("bootstrap-lightbox.css");
             cssOrder.Files.Add("bootstrap-overrides.css");
+            cssOrder.Files.Add("style.css");
+            cssOrder.Files.Add("new-styles.css");
 
 
             var fineUploadOrder = new BundleFileSetOrdering("FineUploadOrder");
