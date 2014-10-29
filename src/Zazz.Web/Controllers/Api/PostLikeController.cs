24573e7c4b4c1a32df3b23d59ab3214fa98a0dc5
KISHAN VIPULBHAI SHAH <kishanvshah7@gmail.com>
2014-10-29 19:50:08 -0400

    Kishan-Target-1.3.1- APIs for Liking Post and Photos
    src\Zazz.web\Controllers\Api
    
    1) PhotoLikeController.cs
    2) PostLikeController.cs
    3) RemovePhotoLikeController.cs
    4) RemovePostLikeController.cs
    
    *Added API controllers for,
    1) Liking a Photo
    2) Liking a Post
    3) Removing Like from a Photo
    4) Removing Like form a Post

---
+++ src/Zazz.Web/Controllers/Api/PostLikeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Interfaces.Services;
+using Zazz.Core.Models.Data;
+using Zazz.Core.Models.Data.Enums;
+using Zazz.Web.Filters;
+using Zazz.Web.Interfaces;
+using Zazz.Web.Models.Api;
+
+namespace Zazz.Web.Controllers.Api
+{
+    [OAuth2Authorize]
+    public class PostLikeController : BaseApiController
+    {
+
+        private readonly IObjectMapper _objectMapper;
+        private readonly ILikeService _likeService;
+        private readonly IUserService _userService;
+
+        public PostLikeController(IUserService userService, ILikeService likeService,
+            IObjectMapper objectMapper)
+        {
+            _likeService = likeService;
+            _objectMapper = objectMapper;
+            _userService = userService;
+        }
+
+
+        // GET /api/v1/like/posts/5
+        public void Get(int id)
+        {
+            _likeService.AddPhotoLike(id, CurrentUserId);
+        }
+
+        // PPOST /api/v1/like/posts/5
+        public void Post(int id)
+        {
+            
+        }
+
+        // PUT api/postlike/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE /api/v1/like/posts/5
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
---
+++ src/Zazz.Web/Controllers/Api/RemovePostLikeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zazz.Core.Interfaces;
+using Zazz.Core.Interfaces.Services;
+using Zazz.Core.Models.Data;
+using Zazz.Core.Models.Data.Enums;
+using Zazz.Web.Filters;
+using Zazz.Web.Interfaces;
+using Zazz.Web.Models.Api;
+
+
+namespace Zazz.Web.Controllers.Api
+{
+    public class RemovePostLikeController : BaseApiController
+    {
+        private readonly IObjectMapper _objectMapper;
+        private readonly ILikeService _likeService;
+        private readonly IUserService _userService;
+
+        public RemovePostLikeController(IUserService userService, ILikeService likeService,
+            IObjectMapper objectMapper)
+        {
+            _likeService = likeService;
+            _objectMapper = objectMapper;
+            _userService = userService;
+        }
+
+        // GET api/removepostlike/5
+        public void Get(int id)
+        {
+            _likeService.RemovePhotoLike(id, CurrentUserId);
+        }
+
+
+        public void Post(int id)
+        {
+
+        }
+
+
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
