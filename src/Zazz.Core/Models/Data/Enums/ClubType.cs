68ffefedffcdd46fdd3b507a2e560776cee25347
Logan Martel <logan.martel@outlook.com>
2015-07-28 16:49:36 -0400

    major UI update

--- src/Zazz.Core/Models/Data/Enums/ClubType.cs
+++ src/Zazz.Core/Models/Data/Enums/ClubType.cs
@@ -4,14 +4,15 @@ namespace Zazz.Core.Models.Data.Enums
 {
     public enum ClubType : byte
     {
+        [Display(Name= "Bars")]
         Bar = 1,
-        [Display(Name = "Night Club")]
+        [Display(Name = "Night Clubs")]
         Nightclub = 2,
-        [Display(Name = "Event Venue")]
+        [Display(Name = "Event Venues")]
         EventVenue = 4,
-        [Display(Name = "Nightlife & Entertainment")]
+        [Display(Name = "Nightlife & Ent.")]
         NightlifeEntertainment = 5,
-        [Display(Name = "Student Club")]
+        [Display(Name = "Student Clubs")]
         StudentClub = 6
 
     }
